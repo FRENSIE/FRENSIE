aba33df06d133d548485049c654ba52e2d544ff3
Alex P. Robinson <aprobinson@wisc.edu>
2014-07-09 13:46:32 -0600

    Moved the sampling procedure for outgoing angle to the neutron scattering distribution base class.

--- packages/facemc/collision/native/src/Facemc_ElasticNeutronScatteringDistribution.cpp
+++ packages/facemc/collision/native/src/Facemc_ElasticNeutronScatteringDistribution.cpp
@@ -13,7 +13,6 @@
 #include "Facemc_ElasticNeutronScatteringDistribution.hpp"
 #include "Utility_DirectionHelpers.hpp"
 #include "Utility_ContractException.hpp"
-#include "Utility_SearchAlgorithms.hpp"
 #include "Utility_SortAlgorithms.hpp"
 
 namespace Facemc{
@@ -74,7 +73,8 @@ void ElasticNeutronScatteringDistribution::scatterNeutron(
 
   // Sample the scattering angle 
   double cm_scattering_angle_cosine = 
-    sampleCMScatteringAngleCosine( neutron.getEnergy() );
+    sampleScatteringAngleCosine( neutron.getEnergy(),
+				 d_angular_scattering_distribution );
 
   // Rotate the neutron velocity vector to the new angle
   // Note: The speed of the neutron does not change in the center-of-mass
@@ -106,59 +106,60 @@ void ElasticNeutronScatteringDistribution::scatterNeutron(
 }
 
 // Sample a center-of-mass scattering angle cosine
-double ElasticNeutronScatteringDistribution::sampleCMScatteringAngleCosine(
-						    const double energy ) const
-{
-  double cm_scattering_angle_cosine;
+// double ElasticNeutronScatteringDistribution::sampleCMScatteringAngleCosine(
+// 						    const double energy ) const
+// {
+//   double cm_scattering_angle_cosine;
 
-  if( energy < d_angular_scattering_distribution.front().first )
-  {
-    cm_scattering_angle_cosine = 
-      d_angular_scattering_distribution.front().second->sample();
-  }
-  else if( energy >= d_angular_scattering_distribution.back().first )
-  {
-    cm_scattering_angle_cosine = 
-      d_angular_scattering_distribution.back().second->sample();
-  }
-  else
-  {
-    Teuchos::Array<Utility::Pair<double,Teuchos::RCP<Utility::OneDDistribution> > >::const_iterator
-      lower_bin_boundary, upper_bin_boundary;
+//   if( energy < d_angular_scattering_distribution.front().first )
+//   {
+//     cm_scattering_angle_cosine = 
+//       d_angular_scattering_distribution.front().second->sample();
+//   }
+//   else if( energy >= d_angular_scattering_distribution.back().first )
+//   {
+//     cm_scattering_angle_cosine = 
+//       d_angular_scattering_distribution.back().second->sample();
+//   }
+//   else
+//   {
+//     Teuchos::Array<Utility::Pair<double,Teuchos::RCP<Utility::OneDDistribution> > >::const_iterator
+//       lower_bin_boundary, upper_bin_boundary;
     
-    lower_bin_boundary = d_angular_scattering_distribution.begin();
-    upper_bin_boundary = d_angular_scattering_distribution.end();
+//     lower_bin_boundary = d_angular_scattering_distribution.begin();
+//     upper_bin_boundary = d_angular_scattering_distribution.end();
     
-    lower_bin_boundary = Utility::Search::binaryLowerBound<Utility::FIRST>( 
-							  lower_bin_boundary,
-							  upper_bin_boundary,
-							  energy );
+//     lower_bin_boundary = Utility::Search::binaryLowerBound<Utility::FIRST>( 
+// 							  lower_bin_boundary,
+// 							  upper_bin_boundary,
+// 							  energy );
 
-    upper_bin_boundary = lower_bin_boundary;
-    ++upper_bin_boundary;
+//     upper_bin_boundary = lower_bin_boundary;
+//     ++upper_bin_boundary;
     
-    double lower_bin_boundary_probability = 
-      (upper_bin_boundary->first - energy)/
-      (upper_bin_boundary->first - lower_bin_boundary->first);
+//     double lower_bin_boundary_probability = 
+//       (upper_bin_boundary->first - energy)/
+//       (upper_bin_boundary->first - lower_bin_boundary->first);
     
-    if( Utility::RandomNumberGenerator::getRandomNumber<double>() < 
-	lower_bin_boundary_probability )
-      cm_scattering_angle_cosine = lower_bin_boundary->second->sample();
-    else
-      cm_scattering_angle_cosine = upper_bin_boundary->second->sample();
-  }
+//     if( Utility::RandomNumberGenerator::getRandomNumber<double>() < 
+// 	lower_bin_boundary_probability )
+//       cm_scattering_angle_cosine = lower_bin_boundary->second->sample();
+//     else
+//       cm_scattering_angle_cosine = upper_bin_boundary->second->sample();
+//   }
 
-  // Due to floating-point roundoff, it is possible for the scattering angle
-  // cosine to be outside [-1,1]. When this occurs, manually set to -1 or 1.
-  if(Teuchos::ScalarTraits<double>::magnitude(cm_scattering_angle_cosine) >1.0)
-    cm_scattering_angle_cosine = copysign( 1.0, cm_scattering_angle_cosine );
+//   // Due to floating-point roundoff, it is possible for the scattering angle
+//   // cosine to be outside [-1,1]. When this occurs, manually set to -1 or 1.
+//   if(Teuchos::ScalarTraits<double>::magnitude(cm_scattering_angle_cosine) >1.0)
+//     cm_scattering_angle_cosine = copysign( 1.0, cm_scattering_angle_cosine );
 
-  // Make sure that the scattering angle cosine is valid
-  testPrecondition( cm_scattering_angle_cosine >= -1.0 );
-  testPrecondition( cm_scattering_angle_cosine <= 1.0 );
+//   // Make sure that the scattering angle cosine is valid
+//   testPrecondition( cm_scattering_angle_cosine >= -1.0 );
+//   testPrecondition( cm_scattering_angle_cosine <= 1.0 );
 
-  return cm_scattering_angle_cosine;
-}								    
+//   return cm_scattering_angle_cosine;
+// }				
+  
 
 } // end Facemc namespace
 
