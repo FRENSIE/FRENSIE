833536dfddcf61ef321b865f9bd1efc3668115fa
Alex Philip Robinson <aprobinson@wisc.edu>
2014-07-17 17:58:03 -0600

    Refactored the NeutronScatteringEnergyDistribution to handle the special case for ENDF Law 44.

--- packages/facemc/collision/native/src/Facemc_NeutronScatteringEnergyDistribution.hpp
+++ packages/facemc/collision/native/src/Facemc_NeutronScatteringEnergyDistribution.hpp
@@ -9,6 +9,9 @@
 #ifndef FACEMC_NEUTRON_SCATTERING_ENERGY_DISTRIBUTION
 #define FACEMC_NEUTRON_SCATTERING_ENERGY_DISTRIBUTION
 
+// Std Lib Includes
+#include <limits>
+
 namespace Facemc{
 
 //! The energy scattering distribution base class
@@ -30,6 +33,10 @@ public:
   //! Sample a scattering energy
   virtual double sampleEnergy( const double energy ) const = 0;
 
+  //! Sample a scattering energy (and return the sampled bin index)
+  virtual double sampleEnergy( const double energy, 
+			       unsigned& sampled_bin_index ) const;
+
 private:
 
   // Records if the energy distribution is for the cm energy
@@ -40,6 +47,16 @@ private:
 inline bool NeutronScatteringEnergyDistribution::isCMDistribution() const
 {
   return d_is_cm_dist;
+}
+
+// Sample a scattering energy (and return the sampled bin index)
+inline double NeutronScatteringEnergyDistribution::sampleEnergy( 
+					    const double energy, 
+					    unsigned& sampled_bin_index ) const
+{
+  sampled_bin_index = std::numeric_limits<unsigned>::max();
+  
+  return this->sampleEnergy( energy );
 }
 
 } // end Facemc namespace
