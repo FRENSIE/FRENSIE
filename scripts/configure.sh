3af2b59b84259c1cc20f660fda821d62ed39ac99
Alex Robinson <aprobinson@wisc.edu>
2014-09-01 17:14:15 -0600

    Updated the configure script and moved the publications to a new repo (FRENSIE_PUBLICATIONS)

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -9,7 +9,8 @@ MOAB_PREFIX_PATH=$HOME/software/moab
 HDF5_PREFIX_PATH=$HOME/software/hdf5
 ODEPACK_PREFIX_PATH=$HOME/software/odepack
 BOOST_PREFIX_PATH=$HOME/software/boost
-FACEMC_PATH=$HOME/summer_2014/transmutation/FRENSIE/
+GSL_PREFIX_PATH=$HOME/software/gsl
+FACEMC_PATH=$HOME/research/transport/FRENSIE/
 
 ##---------------------------------------------------------------------------##
 ## The Teuchos_TwoDArray class has a bug that needs to be fixed. This patch
@@ -23,20 +24,20 @@ rm -rf CMakeCache.txt
 
 ##---------------------------------------------------------------------------##
 
-/home/asb/software/cmake/bin/cmake \
+cmake \
     -D CMAKE_INSTALL_PREFIX:PATH=$PWD/../ \
     -D CMAKE_BUILD_TYPE:STRING=DEBUG \
     -D CMAKE_VERBOSE_CONFIGURE:BOOL=ON \
     -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     -D FRENSIE_ENABLE_DBC:BOOL=ON \
+    -D FRENSIE_ENABLE_OPENMP:BOOL=ON \
+    -D FRENSIE_ENABLE_MPI:BOOL=ON \
     -D TRILINOS_PREFIX:PATH=$TRILINOS_PREFIX_PATH \
     -D MOAB_PREFIX:PATH=$MOAB_PREFIX_PATH \
     -D HDF5_PREFIX:PATH=$HDF5_PREFIX_PATH \
     -D ODEPACK_PREFIX:PATH=$ODEPACK_PREFIX_PATH \
     -D BOOST_PREFIX:PATH=$BOOST_PREFIX_PATH \
-    -D CMAKE_CXX_COMPILER:FILEPATH=/home/asb/software/gcc/bin/c++ \
-    -D CMAKE_C_COMPILER:FILEPATH=/home/asb/software/gcc/bin/gcc \
-    -D CMAKE_Fortran_COMPILER:FILEPATH=/home/asb/software/gcc/bin/gfortran \
+    -D GSL_PREFIX:PATH=$GSL_PREFIX_PATH \
     $EXTRA_ARGS \
     $FACEMC_PATH
 
