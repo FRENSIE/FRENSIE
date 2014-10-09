a12aef23820813080ee50674ea143283e97029b4
Luke <lkersting@wisc.edu>
2014-10-09 10:47:13 -0500

    Added ElectronState test and Electron test to ParticleBank

--- packages/monte_carlo/core/test/tstElectronState.cpp
+++ packages/monte_carlo/core/test/tstElectronState.cpp
@@ -28,5 +28,230 @@ TEUCHOS_UNIT_TEST( ElectronState, getParticleType )
 }
 
 //---------------------------------------------------------------------------//
+// Get the particle speed
+TEUCHOS_UNIT_TEST( ElectronState, getSpeed )
+{
+  MonteCarlo::ElectronState particle( 1ull );
+
+  particle.setEnergy( 1.0 );
+ 
+  double speed_of_light = Utility::PhysicalConstants::speed_of_light;
+  double rest_mass = Utility::PhysicalConstants::electron_rest_mass_energy;
+ 
+  TEST_FLOATING_EQUALITY( particle.getSpeed(),
+                          speed_of_light * sqrt( 1.0 - rest_mass * rest_mass /
+                          ((1.0 + rest_mass) * (1.0 + rest_mass))),
+                          1e-12 );
+}
+
+//---------------------------------------------------------------------------//
+// Set the particle speed
+TEUCHOS_UNIT_TEST( ElectronState, setSpeed )
+{
+  MonteCarlo::ElectronState particle( 1ull );
+
+  double speed_of_light = Utility::PhysicalConstants::speed_of_light;
+  double rest_mass = Utility::PhysicalConstants::electron_rest_mass_energy;
+ 
+  particle.setSpeed( speed_of_light * sqrt( 1 - rest_mass * rest_mass /
+                     ((1 + rest_mass) * (1 + rest_mass))) );
+
+  TEST_FLOATING_EQUALITY( particle.getEnergy(), 1.0, 1e-12 );
+}
+
+//---------------------------------------------------------------------------//
+// Advance the particle along its direction by a specified distance
+TEUCHOS_UNIT_TEST( ElectronState, advance )
+{
+  const double position[3] = {1.0, 1.0, 1.0};
+  const double direction[3] = {0.5773502691896258, 
+			       0.5773502691896258,
+			       0.5773502691896258};
+  
+  MonteCarlo::ElectronState particle( 1ull );
+  particle.setPosition( position );
+  particle.setDirection( direction );
+  particle.setEnergy( 1.0 );
+  particle.setTime( 0.0 );
+
+  particle.advance( 1.7320508075688772 );
+
+  double speed_of_light = Utility::PhysicalConstants::speed_of_light;
+  double rest_mass = Utility::PhysicalConstants::electron_rest_mass_energy;
+ 
+  TEST_FLOATING_EQUALITY( particle.getXPosition(), 2.0, 1e-12 );
+  TEST_FLOATING_EQUALITY( particle.getYPosition(), 2.0, 1e-12 );
+  TEST_FLOATING_EQUALITY( particle.getZPosition(), 2.0, 1e-12 );
+  TEST_FLOATING_EQUALITY( particle.getTime(), 
+                          1.7320508075688772 / ( speed_of_light * sqrt( 1 - rest_mass * rest_mass /
+                          ((1 + rest_mass) * (1 + rest_mass)))), 
+                          1e-12 );
+}
+
+//---------------------------------------------------------------------------//
+// Create new particles
+TEUCHOS_UNIT_TEST( ElectronState, copy_constructor )
+{
+  MonteCarlo::ElectronState particle_gen_a( 1ull );
+  particle_gen_a.setPosition( 1.0, 1.0, 1.0 );
+  particle_gen_a.setDirection( 0.0, 0.0, 1.0 );
+  particle_gen_a.setEnergy( 1.0 );
+  particle_gen_a.setTime( 1.0 );
+  particle_gen_a.incrementCollisionNumber();
+  particle_gen_a.setWeight( 0.5 );
+
+  MonteCarlo::ElectronState particle_gen_a_copy( particle_gen_a );
+
+  TEST_EQUALITY( particle_gen_a_copy.getXPosition(), 
+		 particle_gen_a.getXPosition() );
+  TEST_EQUALITY( particle_gen_a_copy.getYPosition(), 
+		 particle_gen_a.getYPosition() );
+  TEST_EQUALITY( particle_gen_a_copy.getZPosition(), 
+		 particle_gen_a.getZPosition() );
+  TEST_EQUALITY( particle_gen_a_copy.getXDirection(), 
+		 particle_gen_a.getXDirection() );
+  TEST_EQUALITY( particle_gen_a_copy.getYDirection(), 
+		 particle_gen_a.getYDirection() );
+  TEST_EQUALITY( particle_gen_a_copy.getZDirection(), 
+		 particle_gen_a.getZDirection() );
+  TEST_EQUALITY( particle_gen_a_copy.getEnergy(),
+		 particle_gen_a.getEnergy() );
+  TEST_EQUALITY( particle_gen_a_copy.getSpeed(),
+		 particle_gen_a.getSpeed() );
+  TEST_EQUALITY( particle_gen_a_copy.getTime(),
+		 particle_gen_a.getTime() );
+  TEST_EQUALITY( particle_gen_a_copy.getCollisionNumber(),
+		 particle_gen_a.getCollisionNumber() );
+  TEST_EQUALITY( particle_gen_a_copy.getGenerationNumber(),
+		 particle_gen_a.getGenerationNumber() );
+  TEST_EQUALITY( particle_gen_a_copy.getWeight(),
+		 particle_gen_a.getWeight() );
+
+  //Create a second generation particle with the same collision number
+  MonteCarlo::ElectronState particle_gen_b( particle_gen_a, true );
+
+  TEST_EQUALITY( particle_gen_b.getXPosition(), 
+		 particle_gen_a.getXPosition() );
+  TEST_EQUALITY( particle_gen_b.getYPosition(), 
+		 particle_gen_a.getYPosition() );
+  TEST_EQUALITY( particle_gen_b.getZPosition(), 
+		 particle_gen_a.getZPosition() );
+  TEST_EQUALITY( particle_gen_b.getXDirection(), 
+		 particle_gen_a.getXDirection() );
+  TEST_EQUALITY( particle_gen_b.getYDirection(), 
+		 particle_gen_a.getYDirection() );
+  TEST_EQUALITY( particle_gen_b.getZDirection(), 
+		 particle_gen_a.getZDirection() );
+  TEST_EQUALITY( particle_gen_b.getEnergy(),
+		 particle_gen_a.getEnergy() );
+  TEST_EQUALITY( particle_gen_b.getSpeed(),
+		 particle_gen_a.getSpeed() );
+  TEST_EQUALITY( particle_gen_b.getTime(),
+		 particle_gen_a.getTime() );
+  TEST_EQUALITY( particle_gen_b.getCollisionNumber(),
+		 particle_gen_a.getCollisionNumber() );
+  TEST_EQUALITY( particle_gen_b.getGenerationNumber(),
+		 particle_gen_a.getGenerationNumber()+1u );
+  TEST_EQUALITY( particle_gen_b.getWeight(),
+		 particle_gen_a.getWeight() );  
+
+  // Create a third generation particle and reset the collision counter
+  MonteCarlo::ElectronState particle_gen_c( particle_gen_b, true, true );
+
+  TEST_EQUALITY( particle_gen_c.getXPosition(), 
+		 particle_gen_b.getXPosition() );
+  TEST_EQUALITY( particle_gen_c.getYPosition(), 
+		 particle_gen_b.getYPosition() );
+  TEST_EQUALITY( particle_gen_c.getZPosition(), 
+		 particle_gen_b.getZPosition() );
+  TEST_EQUALITY( particle_gen_c.getXDirection(), 
+		 particle_gen_b.getXDirection() );
+  TEST_EQUALITY( particle_gen_c.getYDirection(), 
+		 particle_gen_b.getYDirection() );
+  TEST_EQUALITY( particle_gen_c.getZDirection(), 
+		 particle_gen_b.getZDirection() );
+  TEST_EQUALITY( particle_gen_c.getEnergy(),
+		 particle_gen_b.getEnergy() );
+  TEST_EQUALITY( particle_gen_c.getSpeed(),
+		 particle_gen_b.getSpeed() );
+  TEST_EQUALITY( particle_gen_c.getTime(),
+		 particle_gen_b.getTime() );
+  TEST_EQUALITY_CONST( particle_gen_c.getCollisionNumber(), 0u );
+  TEST_EQUALITY( particle_gen_c.getGenerationNumber(),
+		 particle_gen_b.getGenerationNumber()+1u );
+  TEST_EQUALITY( particle_gen_c.getWeight(),
+		 particle_gen_b.getWeight() );
+}
+
+//---------------------------------------------------------------------------//
+// Create new particles
+TEUCHOS_UNIT_TEST( ElectronState, core_constructor )
+{
+  MonteCarlo::ParticleStateCore core( 1ull, 
+				  MonteCarlo::ELECTRON, 
+				  1.0, 1.0, 1.0,
+				  0.0, 1.0, 0.0,
+				  2.0,
+				  0.5,
+				  1u,
+				  2u,
+				  0.25 );
+
+  MonteCarlo::ElectronState particle( core );
+
+  TEST_EQUALITY_CONST( particle.getHistoryNumber(), 1ull );
+  TEST_EQUALITY_CONST( particle.getParticleType(), MonteCarlo::ELECTRON );
+  TEST_EQUALITY_CONST( particle.getXPosition(), 1.0 );
+  TEST_EQUALITY_CONST( particle.getYPosition(), 1.0 );
+  TEST_EQUALITY_CONST( particle.getZPosition(), 1.0 );
+  TEST_EQUALITY_CONST( particle.getXDirection(), 0.0 );
+  TEST_EQUALITY_CONST( particle.getYDirection(), 1.0 );
+  TEST_EQUALITY_CONST( particle.getZDirection(), 0.0 );
+  TEST_EQUALITY_CONST( particle.getEnergy(), 2.0 );
+  TEST_EQUALITY_CONST( particle.getTime(), 0.5 );
+  TEST_EQUALITY_CONST( particle.getCollisionNumber(), 1u );
+  TEST_EQUALITY_CONST( particle.getGenerationNumber(), 2u );
+  TEST_EQUALITY_CONST( particle.getWeight(), 0.25 );
+}
+
+//---------------------------------------------------------------------------//
+// Create new particles
+TEUCHOS_UNIT_TEST( ElectronState, assignment_operator )
+{
+  MonteCarlo::ElectronState particle_gen_a( 1ull );
+  particle_gen_a.setPosition( 1.0, 1.0, 1.0 );
+  particle_gen_a.setDirection( 0.0, 0.0, 1.0 );
+  particle_gen_a.setEnergy( 1.0 );
+  particle_gen_a.setTime( 1.0 );
+  particle_gen_a.incrementCollisionNumber();
+  particle_gen_a.setWeight( 0.5 );
+
+  MonteCarlo::ElectronState particle_gen_a_copy = particle_gen_a;
+
+  TEST_EQUALITY( particle_gen_a_copy.getXPosition(), 
+		 particle_gen_a.getXPosition() );
+  TEST_EQUALITY( particle_gen_a_copy.getYPosition(), 
+		 particle_gen_a.getYPosition() );
+  TEST_EQUALITY( particle_gen_a_copy.getZPosition(), 
+		 particle_gen_a.getZPosition() );
+  TEST_EQUALITY( particle_gen_a_copy.getXDirection(), 
+		 particle_gen_a.getXDirection() );
+  TEST_EQUALITY( particle_gen_a_copy.getYDirection(), 
+		 particle_gen_a.getYDirection() );
+  TEST_EQUALITY( particle_gen_a_copy.getZDirection(), 
+		 particle_gen_a.getZDirection() );
+  TEST_EQUALITY( particle_gen_a_copy.getEnergy(),
+		 particle_gen_a.getEnergy() );
+  TEST_EQUALITY( particle_gen_a_copy.getTime(),
+		 particle_gen_a.getTime() );
+  TEST_EQUALITY( particle_gen_a_copy.getCollisionNumber(),
+		 particle_gen_a.getCollisionNumber() );
+  TEST_EQUALITY( particle_gen_a_copy.getGenerationNumber(),
+		 particle_gen_a.getGenerationNumber() );
+  TEST_EQUALITY( particle_gen_a_copy.getWeight(),
+		 particle_gen_a.getWeight() );
+}
+
+//---------------------------------------------------------------------------//
 // end tstElectronState.cpp
 //---------------------------------------------------------------------------//
