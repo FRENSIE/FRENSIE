aa026fb39bd4a053766988383e9374dba20fd7f5
kliberty <kliberty@umich.edu>
2014-10-13 18:08:28 -0400

    Use symlinks to avoid weird behavior from removing the CWD while we're in it

--- scripts/init_tree.py
+++ scripts/init_tree.py
@@ -5,18 +5,20 @@ def main():
     cwd = os.getcwd()
     if not cwd.endswith(os.path.join('FRENSIE', 'scripts')):
         print 'This script must be run in \"FRENSIE/scipts\"'
+        print 'Your CWD is', cwd
         return 1
     
     os.chdir('../../')
     os.mkdir('frensie_build_tree')
-    os.renames('FRENSIE', 'frensie_build_tree/FRENSIE')
+    #os.renames('FRENSIE', 'frensie_build_tree/FRENSIE')
+    os.symlink(os.path.abspath('FRENSIE'), 'frensie_build_tree/FRENSIE')
     os.chdir('frensie_build_tree')
     os.symlink('FRENSIE', 'src')
     os.mkdir('deps')
     os.mkdir('deps/install')
     os.mkdir('build')
-    shutil.copyfile('src/makefile.deps', 'deps/makefile')
-    shutil.copyfile('src/frensie.sh', 'build/frensie.sh')   
+    shutil.copyfile('src/scripts/makefile.deps', 'deps/makefile')
+    shutil.copyfile('src/scripts/frensie.sh', 'build/frensie.sh')   
 
     print """
 To build dependencies move necessary tars to deps/tars.
