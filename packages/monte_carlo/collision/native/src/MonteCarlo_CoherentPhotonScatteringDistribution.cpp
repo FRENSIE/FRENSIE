cbd1346bc1309f7b0e60b08fae5b46bae6dcee96
Luke <lkersting@wisc.edu>
2014-10-14 16:07:30 -0500

    Added coherent scattering definition

--- packages/monte_carlo/collision/native/src/MonteCarlo_CoherentPhotonScatteringDistribution.cpp
+++ packages/monte_carlo/collision/native/src/MonteCarlo_CoherentPhotonScatteringDistribution.cpp
@@ -16,21 +16,71 @@ namespace MonteCarlo{
 
 // Constructor
 CoherentPhotonScatteringDistribution::CoherentPhotonScatteringDistribution(
-    const Teuchos::ArrayView<const double>& recoil_electron_momentum_squared,
-    const Teuchos::ArrayView<const double>& integrated_coherent_form_factors )
-  : d_recoil_electron_momentum_squared( recoil_electron_momentum_squared ),
-    d_integrated_coherent_form_factors( integrated_coherent_form_factors )
+   const Teuchos::RCP<Utility::OneDDistribution>& form_factor_function_squared )
+  : d_form_factor_function_squared( form_factor_function_squared )
 {
   // Make sure the arrays are valid
-  testPrecondition( recoil_electron_momentum_squared.size() > 0 );
-  testPrecondition( integrated_coherent_form_factors.size() > 0 );
+  testPrecondition( form_factor_function_squared.size() = 55 );
 }
 
 // Randomly scatter the photon
 void CoherentPhotonScatteringDistribution::scatterPhoton( 
 						    PhotonState& photon ) const
 {
- 
+  // The wavelength of the photon (cm)
+  const double wavelength = ( Utility::PhysicalConstants::planck_constant*
+                              Utility::PhysicalConstants::speed_of_light/
+                              photon.getEnergy() );
+
+  // The squared wavelength of the photon (cm^2)
+  const double wavelength_sqr = wavelength*wavelength;
+
+  // The maximum form factor function squared value
+  const double max_form_factor_function_squared_value = 
+             d_scattering_function->evaluate( 1.0/wavelength_sqr );
+  
+  // The scattering angle cosine
+  double scattering_angle_cosine;
+
+  // The form factor function squared value corresponding to the outgoing angle
+  double form_factor_function_squared_value;
+
+  // The sampled form factor argument squared
+  double form_factor_arg_squared;
+
+  // A random number
+  double random_number;
+
+  // Sample the form factor squared arg from the form factor function squared,
+  // reject with reject function: R( scattering_angle_cosine )
+  do{
+  // Randomly sample the form factor squared
+  double form_factor_arg_squared = d_form_factor_function_squared->
+                               sample( max_form_factor_function_squared_value );
+
+  // Calculate the outgoing photon angle cosine from the sampled form factor 
+  double scattering_angle_cosine = 1.0 - 
+                  2.0*wavelength_sqr*form_factor_arg_sqared;
+
+  random_number = Utility::RandomNumberGenerator::getRandomNumber<double>();
+  }while( random_number > 0.5*( 
+          1.0 + scattering_angle_cosine*scattering_angle_cosine ) );
+
+  // Calculate the outgoing direction
+  double outgoing_photon_direction[3];
+
+  Utility::rotateDirectionThroughPolarAndAzimuthalAngle(
+						   scattering_angle_cosine,
+						   sampleAzimuthalAngle(),
+						   photon.getDirection(),
+						   outgoing_photon_direction );
+
+  // Make sure the scattering angle cosine is valid
+  testPostcondition( scattering_angle_cosine >= -1.0 );
+  testPostcondition( scattering_angle_cosine <= 1.0 );
+
+  // Set the new direction
+  photon.setDirection( outgoing_photon_direction );
 }
 
 } // end MonteCarlo namespace
