b36458c019c01ef159db9fd27c66c10e00a62f32
Alex Robinson <aprobinson@wisc.edu>
2014-10-15 12:29:24 -0500

    Attempting to fix the build issue encountered by @kliberty. Added a new cmake variable called TRILINOS_SOURCE that can be used to specify where the trilinos source directory is located. If set, this will be used to find the Teuchos_StandardUnitTestMain.cpp file used for building simple unit test. The build system will also search  and /lib for the libodepack.a library. @kliberty will need to try the modified build system out on his system before this is pushed into the master. This commit relates to issue #9.

--- cmake/EnableODEPACKSupport.cmake
+++ cmake/EnableODEPACKSupport.cmake
@@ -2,14 +2,20 @@
 # following variables will be set:
 MACRO(ENABLE_ODEPACK_SUPPORT)
 
-  SET(ODEPACK_LIBRARY_DIR ${ODEPACK_PREFIX})
+  # Find the libodepack.a library
+  FIND_PATH(ODEPACK_LIBRARY_DIR
+    NAMES libodepack.a
+    PATHS ${ODEPACK_PREFIX} ${ODEPACK_PREFIX}/lib/)
+  IF(${ODEPACK_LIBRARY_DIR} MATCHES NOTFOUND)
+    MESSAGE(FATAL_ERROR "The libodepack.a file could not be found!")
+  ENDIF()
 
   # Find the ODEPACK library
-#  FIND_LIBRARY(ODEPACK libodepack.a ${ODEPACK_LIBRARY_DIR})
-#  IF(${ODEPACK} MATCHES NOTFOUND)
-#    MESSAGE(FATAL_ERROR "The odepack library could not be found.")
-#  ENDIF(${ODEPACK} MATCHES NOTFOUND)
-  SET(ODEPACK /home/kliberty/frensie/deps/install/odepack/lib/libodepack.a)
+   FIND_LIBRARY(ODEPACK libodepack.a ${ODEPACK_LIBRARY_DIR})
+   IF(${ODEPACK} MATCHES NOTFOUND)
+     MESSAGE(FATAL_ERROR "The odepack library could not be found.")
+   ENDIF(${ODEPACK} MATCHES NOTFOUND)
+  # SET(ODEPACK /home/kliberty/frensie/deps/install/odepack/lib/libodepack.a)
   
   # Set the link paths for HDF5
   LINK_DIRECTORIES(${ODEPACK_LIBRARY_DIR})
