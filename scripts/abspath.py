7ee0d5f21432ea1f06704513e1185b303a2e75dc
kliberty <kliberty@umich.edu>
2014-10-16 00:16:48 -0400

    Better style.

--- scripts/abspath.py
+++ scripts/abspath.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 def main(arg):
-    sys.stdout.write(os.path.abspath(arg))
+    return os.path.abspath(arg)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    sys.stdout.write(main(sys.argv[1]))
 
