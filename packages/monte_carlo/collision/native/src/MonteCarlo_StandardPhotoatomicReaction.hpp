69f76ed7ce1d33822da38621124da04df6d4e856
Alex Robinson <aprobinson@wisc.edu>
2014-10-17 15:33:45 -0500

    Added the pair production reaction and finished unit testing the standard photoatomic reaction class.

--- packages/monte_carlo/collision/native/src/MonteCarlo_StandardPhotoatomicReaction.hpp
+++ packages/monte_carlo/collision/native/src/MonteCarlo_StandardPhotoatomicReaction.hpp
@@ -13,15 +13,23 @@
 #include <Teuchos_ArrayRCP.hpp>
 
 // FRENSIE Includes
-#include "MonteCarlo_StandardPhotoatomicReaction.hpp"
+#include "MonteCarlo_PhotoatomicReaction.hpp"
 #include "MonteCarlo_PhotonState.hpp"
 #include "MonteCarlo_ParticleBank.hpp"
 
 namespace MonteCarlo{
 
-//! The standard photoatomic reaction base class
-template<typename InterpPolicy>
-class StandardPhotoatomicReaction : PhotoatomicReaction
+/*! The standard photoatomic reaction base class
+ * \details Use the InterpPolicy template parameter and the
+ * processed_cross_section template parameter to customize the behavior of
+ * this class. Raw cross section data from the EPDL library would use
+ * a LogLog policy with processed_cross_section = false. Processed cross
+ * section data from an ACE library would use a LogLog policy with
+ * processed_cross_section = true. When data is processed, the policy is used
+ * to indicate how the data was processed.
+ */
+template<typename InterpPolicy, bool processed_cross_section>
+class StandardPhotoatomicReaction : public PhotoatomicReaction
 {
 
 public:
@@ -30,7 +38,7 @@ public:
   StandardPhotoatomicReaction( 
 		  const Teuchos::ArrayRCP<const double>& incoming_energy_grid,
 		  const Teuchos::ArrayRCP<const double>& cross_section,
-		  const double threshold_energy = 0.0 );
+		  const unsigned threshold_energy_index );
 
   //! Destructor
   virtual ~StandardPhotoatomicReaction()
@@ -38,6 +46,9 @@ public:
 
   //! Return the cross section at the given energy
   double getCrossSection( const double energy ) const;
+
+  //! Return the threshold energy
+  double getThresholdEnergy() const;
 
 private:
 
@@ -48,8 +59,43 @@ private:
   Teuchos::ArrayRCP<const double> d_cross_section;
 
   // The threshold energy
-  const double d_threshold_energy;
+  unsigned d_threshold_energy_index;
 };
+
+//! Partial template specialization for raw data
+template<typename InterpPolicy>
+class StandardPhotoatomicReaction<InterpPolicy,false> : public PhotoatomicReaction
+{
+  public:
+
+  //! Constructor
+  StandardPhotoatomicReaction( 
+		  const Teuchos::ArrayRCP<const double>& incoming_energy_grid,
+		  const Teuchos::ArrayRCP<const double>& cross_section,
+		  const unsigned threshold_energy_index );
+
+  //! Destructor
+  virtual ~StandardPhotoatomicReaction()
+  { /* ... */ }
+
+  //! Return the cross section at the given energy
+  double getCrossSection( const double energy ) const;
+
+  //! Return the threshold energy
+  double getThresholdEnergy() const;
+
+private:
+
+  // The incoming energy grid
+  Teuchos::ArrayRCP<const double> d_incoming_energy_grid;
+
+  // The cross section values evaluated on the incoming energy grid
+  Teuchos::ArrayRCP<const double> d_cross_section;
+
+  // The threshold energy
+  const unsigned d_threshold_energy_index;
+};
+
 
 } // end MonteCarlo namespace
 
