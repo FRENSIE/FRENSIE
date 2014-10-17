814484ac0ba3ce03ef45eb6c579a022c56579f0b
Luke <lkersting@wisc.edu>
2014-10-17 14:38:36 -0500

    working on hard scattering class

---
+++ packages/monte_carlo/collision/native/src/MonteCarlo_HardElasticElectronScatteringDistribution.hpp
@@ -0,0 +1,65 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   MonteCarlo_HardElasticElectronScatteringDistribution.hpp
+//! \author Luke Kersting
+//! \brief  The electron hard scattering distribution base class
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef MONTE_CARLO_HARD_ELASTIC_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+#define MONTE_CARLO_HARD_ELASTIC_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+
+// Std Lib Includes
+#include <limits>
+
+// Trilinos Includes
+#include <Teuchos_Array.hpp>
+
+// FRENSIE Includes
+#include "MonteCarlo_ElectronState.hpp"
+#include "MonteCarlo_ParticleBank.hpp"
+#include "Utility_PhysicalConstants.hpp"
+#include "Utility_RandomNumberGenerator.hpp"
+#include "Utility_OneDDistribution.hpp"
+
+namespace MonteCarlo{
+
+//! The scattering distribution base class
+class HardElasticElectronScatteringDistribution : public ElectronScatteringDistribution
+{
+
+public:
+
+  //! Constructor
+  HardElasticElectronScatteringDistribution(
+  const Teuchos::RCP<Utility::OneDDistribution>& elastic_scattering_distribution,
+  const Teuchos::RCP<double>& interpolation_weights );
+
+  //! Destructor
+  virtual ~HardElasticElectronScatteringDistribution()
+  { /* ... */ }
+
+  //! Randomly scatter the electron
+  void scatterElectron( ElectronState& electron,
+	                    ParticleBank& bank) const;
+
+  //! Typedef for the elastic distribution
+  typedef Teuchos::Array<Utility::Pair<double,
+          Teuchos::RCP<Utility::OneDDistribution> > > ElasticDistribution;
+
+private:
+
+  // elastic scattering distribution 
+  const Teuchos::RCP<Utility::OneDDistribution>& d_elastic_scattering_distribution
+
+  // energy grid interpolation fractions for elastic scattering distributions
+  const Teuchos::RCP<double>& d_interpolation_weights
+};
+
+} // end MonteCarlo namespace
+
+#endif // end MONTE_CARLO_HARD_ELASTIC_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_HardElasticElectronScatteringDistribution.hpp
+//---------------------------------------------------------------------------//
