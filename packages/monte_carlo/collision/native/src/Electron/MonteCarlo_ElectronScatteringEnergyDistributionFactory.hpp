7b4401844546498ee4b8930bb4ddcedbeea8a758
Luke kersting <lkersting@wisc.edu>
2014-10-21 09:07:28 -0500

    added tentative empty electron header files

---
+++ packages/monte_carlo/collision/native/src/Electron/MonteCarlo_ElectronScatteringEnergyDistributionFactory.hpp
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file MonteCarlo_ElectronScatteringEnergyDistributionFactory.hpp
+//! \author Luke Kersting
+//! \brief The electron scattering energy distribution factory class declaration
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef MONTE_CARLO_ELECTRON_SCATTERING_ENERGY_DISTRIBUTION_FACTORY_HPP
+#define MONTE_CARLO_ELECTRON_SCATTERING_ENERGY_DISTRIBUTION_FACTORY_HPP
+
+namespace MonteCarlo{
+
+} // end MonteCarlo namespace
+
+#endif // end MONTE_CARLO_ELECTRON_SCATTERING_ENERGY_DISTRIBUTION_FACTORY_HPP
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_ElectronScatteringEnergyDistributionFactory.hpp
+//---------------------------------------------------------------------------//
