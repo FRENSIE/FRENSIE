19af699c5c806b2e2a7e4bb4fc4531045c3301cf
Luke kersting <lkersting@wisc.edu>
2014-10-21 09:07:28 -0500

    added tentative empty electron header files

---
+++ packages/monte_carlo/collision/native/src/Electron/MonteCarlo_ElectronScatteringDistribution.hpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file MonteCarlo_ElectronScatteringDistribution.hpp
+//! \author Luke Kersting
+//! \brief The Electron scattering distribution base class declaration
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef MONTE_CARLO_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+#define MONTE_CARLO_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+
+namespace MonteCarlo{
+
+//! The scattering distribution base class
+class ElectronScatteringDistribution
+{
+
+};
+
+} // end MonteCarlo namespace
+
+#endif // end MONTE_CARLO_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_ElectronScatteringDistribution.hpp
+//---------------------------------------------------------------------------//
---
+++ packages/monte_carlo/collision/native/src/Electron/MonteCarlo_ElectronScatteringEnergyDistribution.hpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file MonteCarlo_ElectronScatteringEnergyDistribution.hpp
+//! \author Luke Kersting
+//! \brief The Electron scattering energy distribution base class declaration
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef MONTE_CARLO_ELECTRON_SCATTERING_ENERGY_DISTRIBUTION_HPP
+#define MONTE_CARLO_ELECTRON_SCATTERING_ENERGY_DISTRIBUTION_HPP
+
+namespace MonteCarlo{
+
+//! The scattering energy distribution base class
+class ElectronScatteringEnergyDistribution
+{
+
+};
+
+} // end MonteCarlo namespace
+
+#endif // end MONTE_CARLO_ELECTRON_SCATTERING_ENERGY_DISTRIBUTION_HPP
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_ElectronScatteringEnergyDistribution.hpp
+//---------------------------------------------------------------------------//
