efa2c9f78330a9634b297c9ec0d4313a6af2cd62
Luke <lkersting@wisc.edu>
2014-10-23 16:07:43 -0500

    Added the standard electroatomic reaction class and definitions

--- packages/monte_carlo/collision/native/src/MonteCarlo_ElectroatomicReaction.hpp
+++ packages/monte_carlo/collision/native/src/MonteCarlo_ElectroatomicReaction.hpp
@@ -1,16 +1,16 @@
 //---------------------------------------------------------------------------//
 //!
-//! \file MonteCarlo_ElectronReaction.hpp
+//! \file MonteCarlo_ElectroAtomicReaction.hpp
 //! \author Luke Kersting
 //! \brief The Electron reaction base class declaration
 //!
 //---------------------------------------------------------------------------//
 
-#ifndef MONTE_CARLO_ELECTRON_REACTION_HPP
-#define MONTE_CARLO_ELECTRON_REACTION_HPP
+#ifndef MONTE_CARLO_ELECTROATOMIC_REACTION_HPP
+#define MONTE_CARLO_ELECTROATOMIC_REACTION_HPP
 
 // FRENSIE Includes
-#include "MonteCarlo_PhotonState.hpp"
+#include "MonteCarlo_ElectronState.hpp"
 #include "MonteCarlo_ParticleBank.hpp"
 
 namespace MonteCarlo{
@@ -22,36 +22,30 @@ class ElectronReaction
 public:
 
   //!Constructor
-  ElectronReaction(
-       const Teuchos::ArrayRCP<const double>& incoming_energy_grid,
-		onst Teuchos::ArrayRCP<const double>& cross_section        );
+  ElectronReaction();
+  { /* ... */ }
 
   //!Destructor
   virtual ~ElectronReaction()
   { /* ... */}
 	
   //! Return the cross section at the given energy
-  double getCrossSection( const double energy ) const;
+  virtual double getCrossSection( const double energy ) const = 0;
+
+  //! Return the threshold energy
+  virtual double getThresholdEnergy() const = 0;
 
   //! Return the number of electrons emitted from the rxn at the given energy
   virtual unsigned getNumberOfEmittedElectrons( const double energy ) const = 0;
 
   //! Simulate the reaction
-  virtual void react( ElectronState& photon, ParticleBank& bank ) const = 0;
-
-private:
-
-  // The incoming energy grid (logarithms)
-  Teuchos::ArrayRCP<const double> d_incoming_energy_grid;
-
-  // The cross section values evaluated on the incoming erg grid (logarithms)
-  Teuchos::ArrayRCP<const double> d_cross_section;
+  virtual void react( ElectronState& electron, ParticleBank& bank ) const = 0;
 };
 
 } // end MonteCarlo namespace
 
-#endif // end MONTE_CARLO_ELECTRON_REACTION_HPP
+#endif // end MONTE_CARLO_ELECTROATOMIC_REACTION_HPP
 
 //---------------------------------------------------------------------------//
-// end MonteCarlo_ElectronReaction.hpp
+// end MonteCarlo_ElectroAtomicReaction.hpp
 //---------------------------------------------------------------------------//
