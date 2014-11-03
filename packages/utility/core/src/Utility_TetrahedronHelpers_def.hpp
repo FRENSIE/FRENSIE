056d95f307b51b291a57f9fcfd053928fe9f2ce1
Eli Moll <ecmartin3@wisc.edu>
2014-11-03 14:37:37 -0600

    Refactoring nomenclature... vertex_d -> reference_vertex

--- packages/utility/core/src/Utility_TetrahedronHelpers_def.hpp
+++ packages/utility/core/src/Utility_TetrahedronHelpers_def.hpp
@@ -23,22 +23,22 @@ template<typename Matrix>
 void calculateBarycentricTransformMatrix( const double vertex_a[3],    
 					  const double vertex_b[3],    
 					  const double vertex_c[3],    
-					  const double vertex_d[3],    
+					  const double reference_vertex[3],    
 					  Matrix& matrix ) 
 {
   // Make sure the matrix is valid
   testPrecondition( matrix.numRows() == 3 );
   testPrecondition( matrix.numCols() == 3 );
   
-  matrix( 0, 0 ) = vertex_a[0] - vertex_d[0];
-  matrix( 0, 1 ) = vertex_b[0] - vertex_d[0];
-  matrix( 0, 2 ) = vertex_c[0] - vertex_d[0];
-  matrix( 1, 0 ) = vertex_a[1] - vertex_d[1];
-  matrix( 1, 1 ) = vertex_b[1] - vertex_d[1];
-  matrix( 1, 2 ) = vertex_c[1] - vertex_d[1];
-  matrix( 2, 0 ) = vertex_a[2] - vertex_d[2];
-  matrix( 2, 1 ) = vertex_b[2] - vertex_d[2];
-  matrix( 2, 2 ) = vertex_c[2] - vertex_d[2];
+  matrix( 0, 0 ) = vertex_a[0] - reference_vertex[0];
+  matrix( 0, 1 ) = vertex_b[0] - reference_vertex[0];
+  matrix( 0, 2 ) = vertex_c[0] - reference_vertex[0];
+  matrix( 1, 0 ) = vertex_a[1] - reference_vertex[1];
+  matrix( 1, 1 ) = vertex_b[1] - reference_vertex[1];
+  matrix( 1, 2 ) = vertex_c[1] - reference_vertex[1];
+  matrix( 2, 0 ) = vertex_a[2] - reference_vertex[2];
+  matrix( 2, 1 ) = vertex_b[2] - reference_vertex[2];
+  matrix( 2, 2 ) = vertex_c[2] - reference_vertex[2];
 
   Teuchos::SerialDenseSolver<typename Matrix::ordinalType,
 			     typename Matrix::scalarType> solver;
