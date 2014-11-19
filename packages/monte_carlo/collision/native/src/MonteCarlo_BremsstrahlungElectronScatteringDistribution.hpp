8a83254d483d0545a06da4691d6ab7e090762de7
Luke <lkersting@wisc.edu>
2014-11-19 17:02:19 -0600

    Wrote bremsstrahlung single scattering class and unit test

---
+++ packages/monte_carlo/collision/native/src/MonteCarlo_BremsstrahlungElectronScatteringDistribution.hpp
@@ -0,0 +1,95 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   MonteCarlo_BremsstrahlungElectronScatteringDistribution.hpp
+//! \author Luke Kersting
+//! \brief  The electron bremsstrahlung scattering distribution base class
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef MONTE_CARLO_BREMSSTRAHLUNG_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+#define MONTE_CARLO_BREMSSTRAHLUNG_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+
+// Std Lib Includes
+#include <limits>
+
+// Boost Includes
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
+// Trilinos Includes
+#include <Teuchos_Array.hpp>
+#include <Teuchos_RCP.hpp>
+
+// FRENSIE Includes
+#include "MonteCarlo_ElectronState.hpp"
+#include "MonteCarlo_ParticleBank.hpp"
+#include "MonteCarlo_ElectronScatteringDistribution.hpp"
+#include "Utility_OneDDistribution.hpp"
+
+namespace MonteCarlo{
+
+//! The scattering distribution base class
+class BremsstrahlungElectronScatteringDistribution : public ElectronScatteringDistribution
+{
+
+public:
+
+  //! Typedef for the  bremsstrahlung distribution
+  typedef Teuchos::Array<Utility::Pair<double,
+                                     Teuchos::RCP<Utility::OneDDistribution> > >
+                                     BremsstrahlungDistribution;
+
+  //! Constructor with simple analytical photon angular distribution
+  BremsstrahlungElectronScatteringDistribution(
+    BremsstrahlungDistribution& bremsstrahlung_scattering_distribution );
+
+  //! Constructor with detailed tabular photon angular distribution
+  BremsstrahlungElectronScatteringDistribution(
+    const double upper_cutoff_energy,
+    const double lower_cutoff_energy,
+    BremsstrahlungDistribution& bremsstrahlung_scattering_distribution,
+    Teuchos::RCP<Utility::OneDDistribution>& angular_distribution );
+
+  //! Destructor 
+  virtual ~BremsstrahlungElectronScatteringDistribution()
+  { /* ... */ }
+
+  //! Randomly scatter the electron
+  void scatterElectron( ElectronState& electron,
+	                    ParticleBank& bank,
+                        unsigned& shell_of_interaction ) const;
+
+private:
+
+  // upper cutoff energy for the condensed-history method
+  double d_upper_cutoff_energy;
+
+  // lower cutoff energy for the condensed-history method
+  double d_lower_cutoff_energy;
+
+  // bremsstrahlung scattering distribution
+  BremsstrahlungDistribution d_bremsstrahlung_scattering_distribution;
+
+  // bremsstrahlung angular distribution of generated photons
+  Teuchos::RCP<Utility::OneDDistribution> d_angular_distribution;
+
+  // Sample the outgoing photon angle for energies above the condensed-history limit
+  inline double SampleDetailedAngle(  double& electron_energy, 
+                                      double& photon_energy ) const ;
+
+  // Sample the outgoing photon angle for energies within the condensed-history limit
+  inline double SampleSimpleAngle(  double& electron_energy, 
+                                    double& photon_energy ) const ;
+
+  // The doppler broadening function pointer
+  boost::function<double ( double&, double& )> d_angular_distribution_func; 
+
+};
+
+} // end MonteCarlo namespace
+
+#endif // end MONTE_CARLO_BREMSSTRAHLUNG_ELECTRON_SCATTERING_DISTRIBUTION_HPP
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_BremsstrahlungElectronScatteringDistribution.hpp
+//---------------------------------------------------------------------------//
--- packages/utility/distribution/src/Utility_TabularDistribution_def.hpp
+++ packages/utility/distribution/src/Utility_TabularDistribution_def.hpp
@@ -34,11 +34,57 @@ TabularDistribution<InterpolationPolicy>::TabularDistribution()
 template<typename InterpolationPolicy>
 TabularDistribution<InterpolationPolicy>::TabularDistribution( 
 			      const Teuchos::Array<double>& independent_values,
-			      const Teuchos::Array<double>& dependent_values )
+			      const Teuchos::Array<double>& dependent_values,
+                  const bool interpret_dependent_values_as_cdf )
   : d_distribution( independent_values.size() ),
     d_norm_constant( 0.0 )
 {
-  initializeDistribution( independent_values, dependent_values );
+  if( interpret_dependent_values_as_cdf )
+  {
+    // Make sure that the bin values are sorted
+    testPrecondition( Sort::isSortedAscending( dependent_values.begin(), 
+					     dependent_values.end() ) );
+
+    // Make sure that for n bin boundaries there are n bin values
+    testPrecondition( independent_values.size() == dependent_values.size() );
+
+    d_distribution[0].first = independent_values[0];
+    d_distribution[0].second = dependent_values[0];
+    // Arbitrarily set the first pdf value to 1.0
+    d_distribution[0].third = 1.0;
+
+    // Assign the distribution
+    for( unsigned i = 1; i < independent_values.size(); ++i )
+    {
+      d_distribution[i].first = independent_values[i];
+      d_distribution[i].second = dependent_values[i];
+
+      // Calculate the pdf from the cdf
+      d_distribution[i].third = -d_distribution[i-1].third + 2.0 *
+        (d_distribution[i].second - d_distribution[i-1].second)/
+        (d_distribution[i].first - d_distribution[i-1].first);
+    }
+
+    // Set normalization constant
+    d_norm_constant = d_distribution.back().second;
+ 
+    // Verify that the CDF is normalized (in event of round-off errors)
+    if( dependent_values.back() != 1.0 )
+    {
+      for( unsigned j = 0; j < d_distribution.size(); ++j )
+      {
+        d_distribution[j].second /= d_norm_constant;
+        d_distribution[j].third /= d_norm_constant;
+      }
+    }
+
+    // Calculate the slopes of the PDF
+    DataProcessor::calculateSlopes<FIRST,THIRD,FOURTH>( d_distribution );
+  }
+  else
+  {
+    initializeDistribution( independent_values, dependent_values );
+  }
 }
 
 // Copy constructor
