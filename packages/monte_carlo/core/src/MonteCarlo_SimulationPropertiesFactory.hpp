c2c68691102dea714fc32267ea712b41e6202c8b
Alex Robinson <aprobinson@wisc.edu>
2014-12-02 15:46:27 -0600

    Finished creating the simulation properties factory class, which will be used to configure and store user run-time options.

---
+++ packages/monte_carlo/core/src/MonteCarlo_SimulationPropertiesFactory.hpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   MonteCarlo_SimulationPropertiesFactory.hpp
+//! \author Alex Robinson
+//! \brief  Simulation properties factory class declaration.
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef MONTE_CARLO_SIMULATION_PROPERTIES_FACTORY_HPP
+#define MONTE_CARLO_SIMULATION_PROPERTIES_FACTORY_HPP
+
+// Std Lib Includes
+#include <stdexcept>
+
+// Trilinos Includes
+#include <Teuchos_ParameterList.hpp>
+
+namespace MonteCarlo{
+
+//! The simulation properties factory
+class SimulationPropertiesFactory
+{
+  
+public:
+  
+  //! Initialize the simulation properties
+  static void initializeSimulationProperties( 
+				    const Teuchos::ParameterList& properties );
+};
+
+} // end MonteCarlo namespace
+
+#endif // end MONTE_CARLO_SIMULATION_PROPERTIES_FACTORY_HPP
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_SimulationPropertiesFactory.hpp
+//---------------------------------------------------------------------------//
