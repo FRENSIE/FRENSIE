be5b8258b8ae3ba502426a9929a08d627e3c19ee
Alex Robinson <aprobinson@wisc.edu>
2014-12-24 01:14:08 -0600

    Updated the ctest dashboard configuration - it now references the cdash.ep.wisc.edu public dashboard host.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,3 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-##
 ## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
@@ -13,3 +9,5 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.ep.wisc.edu")
 set(CTEST_DROP_LOCATION "/submit.php?project=FRENSIE")
 set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE 100000000)
+set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE 100000000)
