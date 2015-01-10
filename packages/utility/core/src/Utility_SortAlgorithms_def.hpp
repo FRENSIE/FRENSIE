44b158325dec601c566f84adcaa4e4ba950b7415
Alex Robinson <aprobinson@wisc.edu>
2015-01-10 13:18:10 -0600

    Test update for ci.

--- packages/utility/core/src/Utility_SortAlgorithms_def.hpp
+++ packages/utility/core/src/Utility_SortAlgorithms_def.hpp
@@ -39,6 +39,7 @@ bool isSortedAscending( Iterator start, Iterator end )
     if( get<member>( *element_a ) > get<member>( *element_b ) )
     {
       is_sorted = false;      
+      
       break;
     }
 
