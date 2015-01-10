468c88743428fe4023ade5365f358e0d13cd0ff4
Luke <lkersting@wisc.edu>
2015-01-10 10:14:42 -0600

    Fixed compile errors in Electroatom ACE Factory

--- packages/monte_carlo/collision/native/src/MonteCarlo_AbsorptionElectroatomicReaction.hpp
+++ packages/monte_carlo/collision/native/src/MonteCarlo_AbsorptionElectroatomicReaction.hpp
@@ -38,6 +38,9 @@ public:
   //! Return the number of electrons emitted from the rxn at the given energy
   unsigned getNumberOfEmittedElectrons( const double energy ) const;
 
+  //! Return the number of photons emitted from the rxn at the given energy
+  unsigned getNumberOfEmittedPhotons( const double energy ) const;
+
   //! Return the reaction type
   ElectroatomicReactionType getReactionType() const;
 
