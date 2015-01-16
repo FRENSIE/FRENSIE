8b10d0035da49a7b538a8a8dbf804913b0c2e363
Luke <lkersting@wisc.edu>
2015-01-15 21:44:57 -0600

    Finished bremsstrahlung scattering unit test for photons and the hard elastic raction unit test

--- packages/monte_carlo/collision/native/test/tstHardElasticElectroatomicReaction.cpp
+++ packages/monte_carlo/collision/native/test/tstHardElasticElectroatomicReaction.cpp
@@ -20,14 +20,13 @@
 #include "Data_XSSEPRDataExtractor.hpp"
 #include "Utility_RandomNumberGenerator.hpp"
 #include "Utility_HistogramDistribution.hpp"
-#include "Utility_TabularDistribution.hpp"
 #include "Utility_UnitTestHarnessExtensions.hpp"
 
 //---------------------------------------------------------------------------//
 // Testing Variables.
 //---------------------------------------------------------------------------//
 
-Teuchos::RCP<MonteCarlo::ElectroatomicReaction> ace_bremsstrahlung_reaction;
+Teuchos::RCP<MonteCarlo::ElectroatomicReaction> ace_elastic_reaction;
 
 //---------------------------------------------------------------------------//
 // Testing Functions.
@@ -43,27 +42,38 @@ bool notEqualZero( double value )
 // Check that the reaction type can be returned
 TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, getReactionType_ace )
 {
-  TEST_EQUALITY_CONST( ace_bremsstrahlung_reaction->getReactionType(),
-		       MonteCarlo::BREMSSTRAHLUNG_ELECTROATOMIC_REACTION );
+  TEST_EQUALITY_CONST( ace_elastic_reaction->getReactionType(),
+		       MonteCarlo::ELASTIC_ELECTROATOMIC_REACTION );
 }
 
 //---------------------------------------------------------------------------//
 // Check that the threshold energy can be returned
 TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, getThresholdEnergy_ace )
 {
-  TEST_EQUALITY_CONST( ace_bremsstrahlung_reaction->getThresholdEnergy(),
-		       exp( -1.381551055796E+01 ) );
+  TEST_EQUALITY_CONST( ace_elastic_reaction->getThresholdEnergy(),
+                       1.000000000000E-05 );
 }
 
 //---------------------------------------------------------------------------//
 // Check that the number of electrons emitted from the rxn can be returned
 TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, getNumberOfEmittedElectrons_ace )
 {
-  TEST_EQUALITY_CONST( ace_bremsstrahlung_reaction->getNumberOfEmittedElectrons(1e-3),
-		       1u );
+  TEST_EQUALITY_CONST( ace_elastic_reaction->getNumberOfEmittedElectrons(1e-3),
+		       0u );
 
-  TEST_EQUALITY_CONST( ace_bremsstrahlung_reaction->getNumberOfEmittedElectrons(20.0),
-		       1u );      
+  TEST_EQUALITY_CONST( ace_elastic_reaction->getNumberOfEmittedElectrons(20.0),
+		       0u );      
+}
+
+//---------------------------------------------------------------------------//
+// Check that the number of photons emitted from the rxn can be returned
+TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, getNumberOfEmittedPhotons_ace )
+{
+  TEST_EQUALITY_CONST( ace_elastic_reaction->getNumberOfEmittedPhotons(1e-3),
+		       0u );
+
+  TEST_EQUALITY_CONST( ace_elastic_reaction->getNumberOfEmittedPhotons(20.0),
+		       0u );      
 }
 
 //---------------------------------------------------------------------------//
@@ -71,23 +81,23 @@ TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, getNumberOfEmittedElectrons
 TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, getCrossSection_ace )
 {
   double cross_section = 
-    ace_bremsstrahlung_reaction->getCrossSection( exp(-1.381551055796E+01 ) );
+    ace_elastic_reaction->getCrossSection( 9.000000000000E-05 );
 
-  TEST_FLOATING_EQUALITY( cross_section, exp( -5.071403810640E+00 ), 1e-12 );
+  TEST_FLOATING_EQUALITY( cross_section, 8.887469904554E+08, 1e-12 );
   
   cross_section =
-    ace_bremsstrahlung_reaction->getCrossSection( exp( -1.364234411496E+01 ) );
+    ace_elastic_reaction->getCrossSection( 4.000000000000E-04 );
   
-  TEST_FLOATING_EQUALITY( cross_section, exp( -4.284251858121E+00 ), 1e-12 );
+  TEST_FLOATING_EQUALITY( cross_section, 4.436635458458E+08, 1e-12 );
 
   cross_section = 
-    ace_bremsstrahlung_reaction->getCrossSection( exp( 1.151292546497E+01 ) );
+    ace_elastic_reaction->getCrossSection( 2.000000000000E-03 );
 
-  TEST_FLOATING_EQUALITY( cross_section, exp( -2.309498238246E+01 ), 1e-12 );
+  TEST_FLOATING_EQUALITY( cross_section, 2.100574153670E+08, 1e-12 );
 }
 
 //---------------------------------------------------------------------------//
-// Check that the bremsstrahlung reaction can be simulated
+// Check that the elastic reaction can be simulated
 TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, react_ace )
 {
   MonteCarlo::ElectronState electron( 0 );
@@ -98,7 +108,7 @@ TEUCHOS_UNIT_TEST( HardElasticElectroatomicReaction, react_ace )
 
   MonteCarlo::SubshellType shell_of_interaction;
 
-  ace_bremsstrahlung_reaction->react( electron, bank, shell_of_interaction );
+  ace_elastic_reaction->react( electron, bank, shell_of_interaction );
 
   TEST_EQUALITY_CONST( electron.getEnergy(), 20.0 );
   TEST_ASSERT( electron.getZDirection() < 1.0 );
@@ -148,52 +158,70 @@ int main( int argc, char** argv )
   Teuchos::ArrayRCP<double> energy_grid;
   energy_grid.deepCopy( xss_data_extractor->extractElectronEnergyGrid() );
   
-  Teuchos::ArrayView<const double> raw_bremsstrahlung_cross_section = 
-    xss_data_extractor->extractHardElasticCrossSection();
+  Teuchos::ArrayView<const double> raw_elastic_cross_section = 
+    xss_data_extractor->extractElasticCrossSection();
   
   Teuchos::ArrayView<const double>::iterator start = 
-    std::find_if( raw_bremsstrahlung_cross_section.begin(),
-                  raw_bremsstrahlung_cross_section.end(),
+    std::find_if( raw_elastic_cross_section.begin(),
+                  raw_elastic_cross_section.end(),
                   notEqualZero );
 
-  Teuchos::ArrayRCP<double> bremsstrahlung_cross_section;
-  bremsstrahlung_cross_section.assign( start, raw_bremsstrahlung_cross_section.end() );
+  Teuchos::ArrayRCP<double> elastic_cross_section;
+  elastic_cross_section.assign( start, raw_elastic_cross_section.end() );
 
-  unsigned bremsstrahlung_threshold_index = 
-    energy_grid.size() - bremsstrahlung_cross_section.size();
+  unsigned elastic_threshold_index = 
+    energy_grid.size() - elastic_cross_section.size();
 
-  // Create the form factor
-  Teuchos::ArrayView<const double>  jcohe_block = 
-    xss_data_extractor->extractJCOHEBlock();
+  // Extract the elastic scattering information data block (ELASI)
+  Teuchos::ArrayView<const double> elasi_block(
+				      xss_data_extractor->extractELASIBlock() );
+  
+  // Extract the number of tabulated distributions
+  int size = elasi_block.size()/3;
 
-  unsigned form_factor_size = jcohe_block.size()/3;
+  // Extract the energy grid for elastic scattering angular distributions
+  Teuchos::Array<double> elastic_energy_grid(elasi_block(0,size));
 
-  Teuchos::Array<double> recoil_momentum_squared( 
-                                          jcohe_block( 0, form_factor_size ) );
+  // Extract the table lengths for elastic scattering angular distributions
+  Teuchos::Array<double> table_length(elasi_block(size,size));
 
-  Teuchos::Array<double> form_factor_squared(
-                         jcohe_block( 2*form_factor_size, form_factor_size ) );
+  // Extract the offsets for elastic scattering angular distributions
+  Teuchos::Array<double> offset(elasi_block(2*size,size));
 
-  for( unsigned i = 0; i < form_factor_size; ++i )
+  // Extract the elastic scattering angular distributions block (elas)
+  Teuchos::ArrayView<const double> elas_block = 
+    xss_data_extractor->extractELASBlock();
+
+  // Create the elastic scattering distributions
+  Teuchos::Array<Utility::Pair<double,Teuchos::RCP<Utility::OneDDistribution> > >
+    elastic_scattering_distribution( size );
+  
+  for( unsigned n = 0; n < size; ++n )
   {
-    recoil_momentum_squared[i] *= 
-      recoil_momentum_squared[i]*1e16; // convert from A^-2 to cm^-2
-    
-    form_factor_squared[i] *= form_factor_squared[i];
-  }
-    
-  Teuchos::RCP<Utility::OneDDistribution> form_factor(
-                             new Utility::TabularDistribution<Utility::LogLog>(
-                                                       recoil_momentum_squared,
-						       form_factor_squared ) );
+    elastic_scattering_distribution[n].first = elastic_energy_grid[n];
+
+    elastic_scattering_distribution[n].second.reset( 
+	  new Utility::HistogramDistribution(
+		 elas_block( offset[n], table_length[n] ),
+		 elas_block( offset[n] + 1 + table_length[n], table_length[n]-1 ),
+         true ) );
+  }  
+
+  // Get the atomic number 
+  const int atomic_number = xss_data_extractor->extractAtomicNumber();
+
+  // Set the cutoff angle cosine 
+  const double cutoff_angle_cosine = 0.999999;
   
   // Create the reaction
-  ace_bremsstrahlung_reaction.reset(
+  ace_elastic_reaction.reset(
 		new MonteCarlo::HardElasticElectroatomicReaction<Utility::LogLog>(
 						      energy_grid,
-						      bremsstrahlung_cross_section,
-						      bremsstrahlung_threshold_index,
-						      form_factor ) );
+						      elastic_cross_section,
+						      elastic_threshold_index,
+						      atomic_number,
+                              cutoff_angle_cosine,
+                              elastic_scattering_distribution ) );
 
   // Clear setup data
   ace_file_handler.reset();
