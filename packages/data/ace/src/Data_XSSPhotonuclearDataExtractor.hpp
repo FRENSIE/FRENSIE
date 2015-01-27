798cfe2a9fdf012b0b59f4755674615e66e76764
Ryan Pease <therolando@aol.com>
2015-01-27 15:06:07 -0600

    Currently working on Photonuclear Table implementaton

---
+++ packages/data/ace/src/Data_XSSPhotonuclearDataExtractor.hpp
@@ -0,0 +1,160 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   Data_XSSPhotonuclearDataExtractor.hpp
+//! \author Ryan Pease
+//! \brief  XSS array (from ace table) photonuclear transport data extractor 
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef DATA_XSS_PHOTONUCLEAR_DATA_EXTRACTOR_HPP
+#define DATA_XSS_PHOTONUCLEAR_DATA_EXTRACTOR_HPP
+
+// Trilinos Includes
+#include <Teuchos_Array.hpp>
+#include <Teuchos_ArrayView.hpp>
+#include <Teuchos_ArrayRCP.hpp>
+
+namespace Data{
+
+/*! \defgroup photonulcear_table Photonuclear Table
+ * \ingroup ace_table
+ * \todo Add reference for table layout.
+ *
+ * After a photonuclear transport table has been read by the 
+ * Data::ACEFileHandler the individual data blocks must be extracted from the
+ * XSS array using the Data::XSSPhotonuclearDataExtractor. Both the NXS array
+ * and the JXS array are required to successfully extract the blocks. The NXS
+ * array elements have the following meanings:
+ * <ul>
+ *  <li> NXS[0] = length of XSS data block</li>
+ *  <li> NXS[1] = ZA=Z*1000+A</li>
+ *  <li> NXS[2] = number of energies in main grid (Ng)</li>
+ *  <li> NXS[3] = number of MT entries n the main energy grid</li>
+ *  <li> NXS[4] = number of secondary particle types with IXS information</li>
+ *  <li> NXS[5] = number of parameter entries (fixed values) in the IXS array
+ *                per secondary particle</li>
+ *  <li> NXS[6] = number of entries (fixed values and locators) in the IXS
+ *                array per secondary particle</li>
+ *  <li> NXS[7-14] = unused (fill with zeros)</li>
+ *  <li> NXS[15] = table format version</li>
+ * </ul>
+ * The JXS array elements have the following meanings:
+ * <ul>
+ *  <li> JXS[0] = main energy grid</li>
+ *  <li> JXS[1] = total cross-section data</li>
+ *  <li> JXS[2] = total nonelastic cross-section data</li>
+ *  <li> JXS[3] = elastic cross-section data</li>
+ *  <li> JXS[4] = total heatng number data</li>
+ *  <li> JXS[5] = MT reaction numbers</li>
+ *  <li> JXS[6] = Q-value reaction energy data</li>
+ *  <li> JXS[7] = cross-section locators (relatve to JXS[8])</li>
+ *  <li> JXS[8] = primary locator for cross-section data</li>
+ *  <li> JXS[9] = first word of IXS array</li>
+ *  <li> JXS[10] = first word of IXS block</li>
+ *  <li> JXS[11-31] = unused (fill with zeros)</li>
+ * </ul>
+ */
+
+/*! The XSS photonuclear data extractor class
+ * \ingroup photonuclear_table
+ */
+class XSSEPRDataExtractor
+{
+  
+public:
+
+  //! Constructor
+  XSSPhotonuclearDataExtractor( const Teuchos::ArrayView<const int>& nxs,
+		       const Teuchos::ArrayView<const int>& jxs,
+		       const Teuchos::ArrayRCP<const double>& xss );
+
+  //! Destructor
+  ~XSSPhotonuclearDataExtractor()
+  { /* ... */ }
+
+  //! Extract the ESZ block
+  Teuchos::ArrayView<const double> extractESZBlock() const;
+
+  //! Extract the TOT block
+  Teuchos::ArrayView<const double> extractTOTBlock() const;
+ 
+  //! Extract the NON block
+  Teuchos::ArrayView<const double> extractNONBlock() const;
+
+  //! Extract the ELS block
+  Teuchos::ArrayView<const double> extractELSBlock() const;
+
+  //! Extract the THN block
+  Teuchos::ArrayView<const double> extractTHNBlock() const;
+
+  //! Extract the MTR block
+  Teuchos::ArrayView<const double> extractMTRBlock() const;
+
+  //! Extract the LQR block
+  Teuchos::ArrayView<const double> extractLQRBlock() const;
+
+  //! Extract the LSIG block
+  Teuchos::ArrayView<const double> extractLSIGBlock() const;
+
+  //! Extract the SIG block
+  Teuchos::ArrayView<const double> extractSIGBlock() const;
+
+  //! Extract the IXS block
+  Teuchos::ArrayView<const double> extractIXSBlock() const;
+
+  //! Extract the IPT block
+  Teuchos::ArrayView<const double> extractIPTBlock() const;
+
+  //! Extract the NTRP block
+  Teuchos::ArrayView<const double> extractNTRPBlock() const;
+
+  //! Extract the PXS block
+  Teuchos::ArrayView<const double> extractPXSBlock() const;
+
+  //! Extract the PHN block
+  Teuchos::ArrayView<const double> extractPHNBlock() const;
+
+  //! Extract the MTRP block
+  Teuchos::ArrayView<const double> extractMTRPBlock() const;
+
+  //! Extract the TYRP block
+  Teuchos::ArrayView<const double> extractTYRPBlock() const;
+
+  //! Extract the LSIGP block
+  Teuchos::ArrayView<const double> extracLSIGPBlock() const;
+
+  //! Extract the SIGP block
+  Teuchos::ArrayView<const double> extractSIGPBlock() const;
+
+  //! Extract the LANDP block
+  Teuchos::ArrayView<const double> extractLANDPBlock() const;
+
+  //! Extract the ANDP block
+  Teuchos::ArrayView<const double> extractANDPBlock() const;
+
+  //! Extract the LDLWP block
+  Teuchos::ArrayView<const double> extractLDLWPBlock() const;
+
+  //! Extract the DLWP block
+  Teuchos::ArrayView<const double> extractDLWPBlock() const;
+
+private:
+
+  // The nxs array (a copy will be stored)
+  Teuchos::Array<int> d_nxs;
+
+  // The jxs array (a copy will be stored so that modifications can be made)
+  Teuchos::Array<int> d_jxs;
+
+  // The xss array (data in this array should never be directly modified)
+  Teuchos::ArrayRCP<const double> d_xss;
+
+};
+
+} // end Data namespace
+
+#endif // end DATA_XSS_PHOTONUCLEAR_DATA_EXTRACTOR_HPP
+
+//---------------------------------------------------------------------------//
+// end Data_XSSPhotonuclearDataExtractor.hpp
+//---------------------------------------------------------------------------//
