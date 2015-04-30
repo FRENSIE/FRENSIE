2e1aad1385e4873875be476c3966552952a4442c
Alex Robinson <aprobinson@wisc.edu>
2015-04-30 14:59:22 -0500

    Added derived photonuclear reaction classes.

--- packages/monte_carlo/collision/native/src/MonteCarlo_PhotonuclearReaction.hpp
+++ packages/monte_carlo/collision/native/src/MonteCarlo_PhotonuclearReaction.hpp
@@ -11,15 +11,18 @@
 
 // Trilinos Includes
 #include <Teuchos_ScalarTraits.hpp>
+#include <Teuchos_RCP.hpp>
 
 // FRENSIE Includes
 #include "MonteCarlo_PhotonuclearReactionType.hpp"
 #include "MonteCarlo_ParticleBank.hpp"
 #include "MonteCarlo_PhotonState.hpp"
+#include "MonteCarlo_NuclearScatteringDistribution.hpp"
 
 namespace MonteCarlo{
 
 //! The photonuclear reaction base class
+template<typename OutgoingParticleType>
 class PhotonuclearReaction
 {
   
@@ -36,7 +39,9 @@ public:
       		   const double q_value,
 		   const unsigned threshold_energy_index,
 		   const Teuchos::ArrayRCP<const double>& incoming_energy_grid,
-		   const Teuchos::ArrayRCP<const double>& cross_section );
+		   const Teuchos::ArrayRCP<const double>& cross_section,
+                   const Teuchos::RCP<const NuclearScatteringDistribution<PhotonState,OutgoingParticleType> >&
+		   outgoing_particle_distribution );
 
   //! Destructor
   virtual ~PhotonuclearReaction()
@@ -52,32 +57,21 @@ public:
   double getThresholdEnergy() const;
 
   //! Return the cross section at a given energy
-  double getCrossSection( const double energy ) const;
+  virtual double getCrossSection( const double energy ) const;
 
-  //! Return the number of photons emitted from the rxn at the given energy
-  virtual unsigned getNumberOfEmittedPhotons( const double energy ) const = 0;
+  //! Return the number of particle emitted from the rxn at the given energy
+  virtual unsigned getNumberOfEmittedParticles( const double energy) const = 0;
 
-  //! Return the average number of photons emitted from the rxn
-  virtual double getAverageNumberOfEmittedPhotons( const double energy ) const;
-
-  //! Return the number of neutrons emitted from the rxn at the given energy
-  virtual unsigned getNumberOfEmittedNeutrons( const double energy) const = 0;
-
-  //! Return the average number of neutrons emitted from the rxn
-  virtual double getAverageNumberOfEmittedNeutrons( const double energy) const;
-
+  //! Return the average number of particles emitted from the rxn
+  virtual double getAverageNumberOfEmittedParticles(const double energy) const;
 
   //! Simulate the reaction
   virtual void react( PhotonState& photon, ParticleBank& bank ) const = 0;
 
 protected:
 
-  //! Return an integer number of emitted photons given a non-integer value
-  unsigned sampleNumberOfEmittedPhotons(
-				     const double average_multiplicity ) const;
-
-  //! Return an integer number of emitted photons given a non-integer value
-  unsigned sampleNumberOfEmittedNeutrons(
+  //! Return an integer number of emitted particles given a non-integer value
+  unsigned sampleNumberOfEmittedParticles(
 				     const double average_multiplicity ) const;
 
 private:
@@ -96,37 +90,40 @@ private:
 
   // The cross section values evaluated on the incoming energy grid
   Teuchos::ArrayRCP<const double> d_cross_section;
+
+  // The outgoing particle distribution (energy and angle)
+  Teuchos::RCP<const NuclearScatteringDistribution<PhotonState,OutgoingParticleType> >
+  d_outgoing_particle_distribution;
 };
 
-// Return the average number of photons emitted from the rxn
+// Return the average number of particles emitted from the rxn
 /*! \details If the neutron multiplicity for the reaction is not an integer
  * at the desired energy, this function should be overridden in the derived
  * class. It will be used in implicit multiplication weighting games.
  */
-  inline double PhotonuclearReaction::getAverageNumberOfEmittedPhotons(
-								       const double energy) const
-  {
-    return this->getNumberOfEmittedPhotons( energy );
-  }
-
-// Return the average number of neutrosn emitted from the rxn
-/*! \details If the neutron multiplicity for the reaction is not an integer
- * at the desired energy, this function should be overridden in the derived
- * class. It will be used in implicit multiplication weighting games.
- */
-  inline double PhotonuclearReaction::getAverageNumberOfEmittedNeutrons(
-								       const double energy) const
-  {
-    return this->getNumberOfEmittedNeutrons( energy );
-  }
+tempate<typename OutgoingParticleType>
+inline double PhotonuclearReaction<OutgoingParticleType>::getAverageNumberOfEmittedParticles(
+						     const double energy) const
+{
+  return this->getNumberOfEmittedParticles( energy );
+}
 
 // Return the threshold energy
-inline double PhotonuclearReaction::getThresholdEnergy() const
+tempate<typename OutgoingParticleType>
+inline double PhotonuclearReaction<OutgoingParticleType>::getThresholdEnergy() const
 {
   return d_incoming_energy_grid[d_threshold_energy_index];
 }
 
 } // end MonteCarlo namespace
+
+//---------------------------------------------------------------------------//
+// Template Includes
+//---------------------------------------------------------------------------//
+
+#include "MonteCarlo_PhotonuclearReaction_def.hpp"
+
+//---------------------------------------------------------------------------//
 
 #endif // end MONTE_CARLO_PHOTONUCLEAR_REACTION_HPP
 
