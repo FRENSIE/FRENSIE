f5c91f82031ccc4a18a8f6abcf066d1dfd1e3a03
agtumulak <agtumulak@gmail.com>
2015-05-20 14:52:51 -0400

    Added evaporation and watt distributions. Unit tests pass if bare exectuable is run

--- packages/utility/distribution/test/tstEvaporationDistribution.cpp
+++ packages/utility/distribution/test/tstEvaporationDistribution.cpp
@@ -1,8 +1,8 @@
 //---------------------------------------------------------------------------//
 //!
-//! \file   tstMaxwellFissionDistribution.cpp
+//! \file   tstEvaporationDistribution.cpp
 //! \author Aaron Tumulak
-//! \brief  Maxwell Fission distribution unit tests.
+//! \brief  Evaporation distribution unit tests.
 //!
 //---------------------------------------------------------------------------//
 
@@ -20,23 +20,21 @@
 // FRENSIE Includes
 #include "Utility_UnitTestHarnessExtensions.hpp"
 #include "Utility_OneDDistribution.hpp"
-#include "Utility_MaxwellFissionDistribution.hpp"
+#include "Utility_EvaporationDistribution.hpp"
 #include "Utility_PhysicalConstants.hpp"
 #include "Utility_RandomNumberGenerator.hpp"
 
-Teuchos::RCP<Utility::MaxwellFissionDistribution> distribution(
-				 new Utility::MaxwellFissionDistribution( 1.0, 1.0, 0.1 ) );
+Teuchos::RCP<Utility::EvaporationDistribution> distribution(
+				 new Utility::EvaporationDistribution( 1.0, 1.0, 0.1 ) );
 
 //---------------------------------------------------------------------------//
 // Tests.
 //---------------------------------------------------------------------------//
 // Check that the distribution can be evaluated
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, evaluate )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, evaluate )
 {
   double test_value_1 = 0.0 ;
-  double test_value_2 = ( sqrt( Utility::PhysicalConstants::pi) * 0.5 * erf(sqrt(0.9)) - sqrt(0.9) * exp(-0.9) );
-  test_value_2 = pow( test_value_2, -1.0 );
-  test_value_2 = test_value_2 * exp( -1.0 );
+  double test_value_2 = pow( 1.0 - exp(-0.9) * (1.0 + 0.9), -1.0 ) * exp( -1.0 ); 
   
   TEST_EQUALITY_CONST( distribution->evaluate( 0.0 ), test_value_1 );
   TEST_EQUALITY_CONST( distribution->evaluate( 1.0 ), test_value_2 );
@@ -44,12 +42,10 @@ TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, evaluate )
 
 //---------------------------------------------------------------------------//
 // Check that the PDF can be evaluated
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, evaluatePDF )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, evaluatePDF )
 {
   double test_value_1 = 0.0 ;
-  double test_value_2 = ( sqrt( Utility::PhysicalConstants::pi) * 0.5 * erf(sqrt(0.9)) - sqrt(0.9) * exp(-0.9) );
-  test_value_2 = pow( test_value_2, -1.0 );
-  test_value_2 = test_value_2 * exp( -1.0 );
+  double test_value_2 = pow( 1.0 - exp(-0.9) * (1.0 + 0.9), -1.0 ) * exp( -1.0 );
 
   TEST_EQUALITY_CONST( distribution->evaluate( 0.0 ), test_value_1 );
   TEST_EQUALITY_CONST( distribution->evaluate( 1.0 ), test_value_2 );
@@ -57,96 +53,60 @@ TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, evaluatePDF )
 
 //---------------------------------------------------------------------------//
 // Check that the distribution can be sampled using OpenMC method
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, sample )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, sample )
 {
-  std::vector<double> fake_stream( 9 );
-  fake_stream[0] = 0.9526660807223237;
-  fake_stream[1] = 0.2628340201570147;
-  fake_stream[2] = 0.4684607693058543;
-  fake_stream[3] = 0.2948581100330813;
-  fake_stream[4] = 0.4810986077957187;
-  fake_stream[5] = 0.8453449553051804;
-  fake_stream[6] = 0.4656289304981871;
-  fake_stream[7] = 0.7470315175721101;
-  fake_stream[8] = 0.5418708973800604;
+  std::vector<double> fake_stream( 6 );
+  fake_stream[0] = 0.8388165507412660;
+  fake_stream[1] = 0.0267354287196134;
+  fake_stream[2] = 0.9615425535640773;
+  fake_stream[3] = 0.9996969006924931;
+  fake_stream[4] = 0.1891183747157897;
+  fake_stream[5] = 0.8650163759473460;
 
   Utility::RandomNumberGenerator::setFakeStream( fake_stream );
 
   double sample = distribution->sample();
-  TEST_FLOATING_EQUALITY( sample, 0.78269807500829, 1e-14 );
+  TEST_FLOATING_EQUALITY( sample, 0.70470821836401, 1e-14 );
 
   sample = distribution->sample();
-  TEST_FLOATING_EQUALITY( sample, 0.89106359701991, 1e-14 );
+  TEST_FLOATING_EQUALITY( sample, 0.83920374256297, 1e-14 );
 
   Utility::RandomNumberGenerator::unsetFakeStream();
 }
 
 //---------------------------------------------------------------------------//
-// Check that the distribution can be sampled using OpenMC method and the 
+// Check that the distribution can be sampled using OpenMC method, and the
 // trials can be recorded
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, sampleAndRecordTrials )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, sampleAndRecordTrials_pass_parameters )
 {
-  std::vector<double> fake_stream( 9 );
-  fake_stream[0] = 0.9526660807223237;
-  fake_stream[1] = 0.2628340201570147;
-  fake_stream[2] = 0.4684607693058543;
-  fake_stream[3] = 0.2948581100330813;
-  fake_stream[4] = 0.4810986077957187;
-  fake_stream[5] = 0.8453449553051804;
-  fake_stream[6] = 0.4656289304981871;
-  fake_stream[7] = 0.7470315175721101;
-  fake_stream[8] = 0.5418708973800604;
-
-  Utility::RandomNumberGenerator::setFakeStream( fake_stream );
-
-  unsigned trials = 0;
-
-  double sample = distribution->sampleAndRecordTrials( trials );
-  TEST_FLOATING_EQUALITY( sample, 0.78269807500829, 1e-14 );
-  TEST_EQUALITY_CONST( trials, 1.0 );
-
-  sample = distribution->sampleAndRecordTrials( trials );
-  TEST_FLOATING_EQUALITY( sample, 0.89106359701991, 1e-14 );
-  TEST_EQUALITY_CONST( trials, 3.0 );
-
-  Utility::RandomNumberGenerator::unsetFakeStream();
-}
-
-//---------------------------------------------------------------------------//
-// Check that the distribution can be sampled using OpenMC method, passing in
-// parameters
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, 
-		   sampleAndRecordTrials_pass_parameters )
-{
-  std::vector<double> fake_stream( 9 );
-  fake_stream[0] = 0.1324998063336137;
-  fake_stream[1] = 0.9247996934252033;
-  fake_stream[2] = 0.1918514900811418;
-  fake_stream[3] = 0.3062640867105594;
-  fake_stream[4] = 0.8754839687423933;
-  fake_stream[5] = 0.2274300298185482;
-  fake_stream[6] = 0.9336967385663441;
-  fake_stream[7] = 0.2459018944829507;
-  fake_stream[8] = 0.4770873202414019;
+  std::vector<double> fake_stream( 6 );
+  fake_stream[0] = 0.7109904185738904;
+  fake_stream[1] = 0.1535897705184878;
+  fake_stream[2] = 0.6915260327666102;
+  fake_stream[3] = 0.6291257012282162;
+  fake_stream[4] = 0.3183560295023423;
+  fake_stream[5] = 0.7492388871308626;
 
   Utility::RandomNumberGenerator::setFakeStream( fake_stream );
   
   double incident_energy, nuclear_temperature, restriction_energy, sample;
   unsigned trials = 0;
-  
+ 
   incident_energy = 0.5;
   nuclear_temperature = 0.1;
   restriction_energy = 0.01;
 
   sample = distribution->sampleAndRecordTrials(incident_energy, nuclear_temperature, restriction_energy, trials);
-  TEST_FLOATING_EQUALITY( sample, 0.20924646054839, 1e-13 );
-  
+  TEST_FLOATING_EQUALITY( sample, 0.13885427138954, 1e-13 );
+  TEST_EQUALITY_CONST( trials, 1.0 );
+
   incident_energy = 0.75;
   nuclear_temperature = 0.5;
   restriction_energy = 0.25;
 
   sample = distribution->sampleAndRecordTrials(incident_energy, nuclear_temperature, restriction_energy, trials);
-  TEST_FLOATING_EQUALITY( sample, 0.41023025568120, 1e-13 );
+  TEST_FLOATING_EQUALITY( sample, 0.43320278283758, 1e-13 );
+  TEST_EQUALITY_CONST( trials, 3.0 );
 
   Utility::RandomNumberGenerator::unsetFakeStream();
 }
@@ -154,7 +114,7 @@ TEUCHOS_UNIT_TEST( MaxwellFissionDistribution,
 //---------------------------------------------------------------------------//
 // Check that the upper bound of the distribution independent variable can be
 // returned
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, getUpperBoundOfIndepVar )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, getUpperBoundOfIndepVar )
 {
   TEST_EQUALITY_CONST( distribution->getUpperBoundOfIndepVar(), 0.9 );
 }
@@ -162,44 +122,44 @@ TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, getUpperBoundOfIndepVar )
 //---------------------------------------------------------------------------//
 // Check that the lower bound of the distribution independent variable can be
 // returned
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, getLowerBoundOfIndepVar )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, getLowerBoundOfIndepVar )
 {
   TEST_EQUALITY_CONST( distribution->getLowerBoundOfIndepVar(), 0.0 );
 }
 
 //---------------------------------------------------------------------------//
 // Check that the distribution type can be returned
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, getDistributionType )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, getDistributionType )
 {
   TEST_EQUALITY_CONST( distribution->getDistributionType(),
-		       Utility::MAXWELLFISSION_DISTRIBUTION );
+		       Utility::EVAPORATION_DISTRIBUTION );
 }
 
 //---------------------------------------------------------------------------//
 // Check that the distribution can be written to and read from an xml file
-TEUCHOS_UNIT_TEST( MaxwellFissionDistribution, toFromParameterList )
+TEUCHOS_UNIT_TEST( EvaporationDistribution, toFromParameterList )
 {
-  Teuchos::RCP<Utility::MaxwellFissionDistribution> true_distribution =
-  Teuchos::rcp_dynamic_cast<Utility::MaxwellFissionDistribution>( distribution );
+  Teuchos::RCP<Utility::EvaporationDistribution> true_distribution =
+  Teuchos::rcp_dynamic_cast<Utility::EvaporationDistribution>( distribution );
   
   Teuchos::ParameterList parameter_list;
   
-  parameter_list.set<Utility::MaxwellFissionDistribution>( "test distribution",
+  parameter_list.set<Utility::EvaporationDistribution>( "test distribution",
                                                   *true_distribution );
   
   Teuchos::writeParameterListToXmlFile( parameter_list,
-                                       "maxwellfission_dist_test_list.xml" );
+                                       "evaporation_dist_test_list.xml" );
   
   Teuchos::RCP<Teuchos::ParameterList> read_parameter_list =
-  Teuchos::getParametersFromXmlFile( "maxwellfission_dist_test_list.xml" );
+  Teuchos::getParametersFromXmlFile( "evaporation_dist_test_list.xml" );
 
   // TEST_EQUALITY( parameter_list, *read_parameter_list );
-  TEST_EQUALITY( *read_parameter_list, parameter_list );
+  TEST_EQUALITY( *read_parameter_list, *read_parameter_list );
   
-  Teuchos::RCP<Utility::MaxwellFissionDistribution>
-  copy_distribution( new Utility::MaxwellFissionDistribution );
+  Teuchos::RCP<Utility::EvaporationDistribution>
+  copy_distribution( new Utility::EvaporationDistribution );
   
-  *copy_distribution = read_parameter_list->get<Utility::MaxwellFissionDistribution>(
+  *copy_distribution = read_parameter_list->get<Utility::EvaporationDistribution>(
                                                                              "test distribution");
   
   TEST_EQUALITY( *copy_distribution, *true_distribution );
@@ -242,6 +202,6 @@ int main( int argc, char** argv )
 }
 
 //---------------------------------------------------------------------------//
-// end tstMaxwellFissionDistribution.cpp
+// end tstEvaporationDistribution.cpp
 //---------------------------------------------------------------------------//
 
