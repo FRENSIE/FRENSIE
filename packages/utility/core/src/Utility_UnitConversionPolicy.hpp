4523ba0eeef730e3deffc2b530f079363dee548a
Alex Robinson <aprobinson@wisc.edu>
2015-06-16 18:11:34 -0500

    Finished the unit converstion policy structs and unit tests.

--- packages/utility/core/src/Utility_UnitConversionPolicy.hpp
+++ packages/utility/core/src/Utility_UnitConversionPolicy.hpp
@@ -10,19 +10,9 @@
 #define UTILITY_UNIT_CONVERSION_POLICY_HPP
 
 // FRENSIE Includes
-#include "Utility_LengthConversionPolicy.hpp"
 #include "Utility_InverseLengthConversionPolicy.hpp"
-
-namespace Utility{
-
-//! The area dimension tag
-typedef boost::units::area_dimension AreaDimension;
-
-//! The inverse area dimension tag
-typedef boost::units::derived_dimension<boost::units::length_base_dimension,-2>::type InverseAreaDimension;
-
-
-} // end Utility namespace
+#include "Utility_InverseAreaConversionPolicy.hpp"
+#include "Utility_UnitlessMomentumConversionPolicy.hpp"
 
 #endif // end UTILITY_UNIT_CONVERSION_POLICY_HPP
 
