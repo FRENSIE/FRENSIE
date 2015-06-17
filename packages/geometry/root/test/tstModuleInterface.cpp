72b07ea3c6e07f3a353b8b3df2f375aaeaa5b7e2
Eli Moll <ecmartin3@wisc.edu>
2015-06-17 18:48:23 -0500

    Alex helping with initial ROOT implementation prior to unit testing completion.

---
+++ packages/geometry/root/test/tstModuleInterface.cpp
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------------//
+//! 
+//! \file   tstModuleInterface.cpp
+//! \author Alex Robinson, Eli Moll
+//! \brief  Geometry module interface unit tests
+//!
+//---------------------------------------------------------------------------//
+
+// Std Lib Includes
+#include <iostream>
+
+// Trilinos Includes
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_UnitTestRepository.hpp>
+#include <Teuchos_GlobalMPISession.hpp>
+#include <Teuchos_Array.hpp>
+#include <Teuchos_Tuple.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include <Teuchos_XMLParameterListCoreHelpers.hpp>
+#include <Teuchos_VerboseObject.hpp>
+
+// FRENSIE Includes
+
+//---------------------------------------------------------------------------//
+// end tstModuleInterface.cpp
+//---------------------------------------------------------------------------//
+
