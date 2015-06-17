fccde0501c311b9467ec6eca978dd7b16e345d79
Eli Moll <ecmartin3@wisc.edu>
2015-06-17 18:48:23 -0500

    Alex helping with initial ROOT implementation prior to unit testing completion.

---
+++ packages/geometry/root/test/tstRootHelpers.cpp
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   tstDagMCHelpers.cpp
+//! \author Alex Robinson, Eli Moll
+//! \brief  DagMC helper function unit tests
+//!
+//---------------------------------------------------------------------------//
+
+// Std Lib Includes
+#include <iostream>
+#include <algorithm>
+
+// Trilinos Includes
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_UnitTestRepository.hpp>
+#include <Teuchos_GlobalMPISession.hpp>
+
+// FRENSIE Includes
+				  
+//---------------------------------------------------------------------------//
+// end tstDagMCHelpers.cpp
+//---------------------------------------------------------------------------//
