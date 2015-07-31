898d505a89a04a1c94fcdc8bc979d790319f5e06
Luke Kersting <lkersting@wisc.edu>
2015-07-31 13:10:07 -0600

    fixed soft elastic data container xml error by removing spaces from names

--- packages/data/native/src/Data_SoftElasticElectronDataContainer_def.hpp
+++ packages/data/native/src/Data_SoftElasticElectronDataContainer_def.hpp
@@ -24,23 +24,23 @@ void SoftElasticElectronDataContainer::save( Archive& ar,
 						  const unsigned version) const
 {
   ar & boost::serialization::make_nvp( "atomic_number", d_atomic_number );
-  ar & boost::serialization::make_nvp( "cutoff angle cosine", 
+  ar & boost::serialization::make_nvp( "cutoff_angle_cosine", 
                                         d_cutoff_angle_cosine );
-  ar & boost::serialization::make_nvp( "angular energy grid",
+  ar & boost::serialization::make_nvp( "angular_energy_grid",
 				                        d_angular_energy_grid );
-  ar & boost::serialization::make_nvp( "number of discrete angles", 
+  ar & boost::serialization::make_nvp( "number_of_discrete_angles", 
                                         d_number_of_discrete_angles );
-  ar & boost::serialization::make_nvp( "soft elastic discrete angles", 
+  ar & boost::serialization::make_nvp( "soft_elastic_discrete_angles", 
                                         d_soft_elastic_discrete_angles );
-  ar & boost::serialization::make_nvp( "soft elastic weights", 
+  ar & boost::serialization::make_nvp( "soft_elastic_weights", 
                                         d_soft_elastic_weights );
-  ar & boost::serialization::make_nvp( "electron energy grid",
+  ar & boost::serialization::make_nvp( "electron_energy_grid",
 				                        d_electron_energy_grid );
   ar & boost::serialization::make_nvp( 
-            "moment preserving soft elastic cross section", 
+            "moment_preserving_soft_elastic_cross_section", 
             d_moment_preserving_soft_elastic_cross_section );
   ar & boost::serialization::make_nvp( 
-            "moment preserving soft elastic cross section threshold index", 
+            "moment_preserving_soft_elastic_cross_section_threshold_index", 
             d_moment_preserving_soft_elastic_cross_section_threshold_index );
 }
   
@@ -50,23 +50,23 @@ void SoftElasticElectronDataContainer::load( Archive& ar,
 						  const unsigned version )
 {
   ar & boost::serialization::make_nvp( "atomic_number", d_atomic_number );
-  ar & boost::serialization::make_nvp( "cutoff angle cosine", 
+  ar & boost::serialization::make_nvp( "cutoff_angle_cosine", 
                                         d_cutoff_angle_cosine );
-  ar & boost::serialization::make_nvp( "angular energy grid",
+  ar & boost::serialization::make_nvp( "angular_energy_grid",
 				                        d_angular_energy_grid );
-  ar & boost::serialization::make_nvp( "number of discrete angles", 
+  ar & boost::serialization::make_nvp( "number_of_discrete_angles", 
                                         d_number_of_discrete_angles );
-  ar & boost::serialization::make_nvp( "soft elastic discrete angles", 
+  ar & boost::serialization::make_nvp( "soft_elastic_discrete_angles", 
                                         d_soft_elastic_discrete_angles );
-  ar & boost::serialization::make_nvp( "soft elastic weights", 
+  ar & boost::serialization::make_nvp( "soft_elastic_weights", 
                                         d_soft_elastic_weights );
-  ar & boost::serialization::make_nvp( "electron energy grid",
+  ar & boost::serialization::make_nvp( "electron_energy_grid",
 				                        d_electron_energy_grid );
   ar & boost::serialization::make_nvp( 
-            "moment preserving soft elastic cross section", 
+            "moment_preserving_soft_elastic_cross_section", 
             d_moment_preserving_soft_elastic_cross_section );
   ar & boost::serialization::make_nvp( 
-            "moment preserving soft elastic cross section threshold index", 
+            "moment_preserving_soft_elastic_cross_section_threshold_index", 
             d_moment_preserving_soft_elastic_cross_section_threshold_index );
 }
 
