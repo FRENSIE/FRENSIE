265a3d49aea4c5f6d7c8d22cde547a66a34fcde3
Eli Moll <ecmartin3@wisc.edu>
2015-08-25 17:17:31 -0500

    Updated with root test directory

--- examples/root_test/Test_Root_Geometry.C
+++ examples/root_test/Test_Root_Geometry.C
@@ -19,30 +19,38 @@ void Test_Root_Geometry()
                    "Geometry for testing root implementation" );
    
   // Define materials and media (space filling materials)
-  TGeoMaterial *void_mat = new TGeoMaterial( "void",0,0,0 );
+  TGeoMaterial *void_mat = new TGeoMaterial( "void",1,1,1 );
   TGeoMedium   *void_med = new TGeoMedium( "void_med",1,void_mat );
    
   TGeoMaterial *mat_1 = new TGeoMaterial( "mat_1",1,1,1 );
   TGeoMedium   *med_1 = new TGeoMedium( "med_1",2,mat_1 );
+  
+  TGeoMaterial *terminal_mat = new TGeoMaterial( "graveyard",0,0,0 );
+  TGeoMedium   *terminal_med = new TGeoMedium( "graveyard",3,terminal_mat );
 
-  // Define the cube volume and set it to be the highest node
+  // Define the graveyard volume and set it to be the highest node
+  TGeoVolume *terminal_cube = gGeoManager->MakeBox( "TERMINAL",
+                                                     terminal_med,
+                                                     7., 7., 7. );
+  gGeoManager->SetTopVolume( terminal_cube );
+  terminal_cube->SetUniqueID(3);
+  
   TGeoVolume *cube = gGeoManager->MakeBox( "CUBE",void_med,5.,5.,5. );
-  gGeoManager->SetTopVolume( cube );
   cube->SetUniqueID(1);
+  cube->SetVisibility(kTRUE);
 
   // Define the spherical volume
   TGeoVolume *sphere = gGeoManager->MakeSphere( "SPHERE",med_1,0.,2.5 );
   sphere->SetUniqueID(2);
 
   // Add the sphere as a daughter of the cube
+  terminal_cube->AddNode( cube, 1 );
   cube->AddNode( sphere, 1 );
   
   // Close the geometry and draw it for visualization
   gGeoManager->CloseGeometry();
-
-  gGeoManager->SetVisLevel(10);
   gGeoManager->SetTopVisible();
-  cube->Draw();
+  terminal_cube->Draw();
 
 }  // end Test_Root_Geometry
 
