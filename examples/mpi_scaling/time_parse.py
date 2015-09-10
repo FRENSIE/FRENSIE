533c46fb57639664367a6e17b7097be88a7ee8ef
Eli Moll <ecmartin3@wisc.edu>
2015-09-10 10:51:02 -0500

    Working to resolve issue with running non mpi facemc. Error revolving around attempting to get communicator size without global MPI initialized.

---
+++ examples/mpi_scaling/time_parse.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+
+# File prefix
+file_prefix = "MPI_Strong_"
+
+# File suffix
+file_suffix = ".txt"
+
+# Array of number of cores for simulations
+core_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+# Output file name
+output_file_name = file_prefix + "Summary" + file_suffix
+
+output_file = open(output_file_name, 'w')
+
+for i in range(0, len(core_numbers)):
+    input_file = open(file_prefix + str(core_numbers[i]) + file_suffix, 'r')
+    
+    found_time = False;
+    time = 0;
+    
+    while ( not found_time ):
+        line = input_file.readline()
+        
+        if (line != None and line.split()[0] == "Simulation" and line.split()[1] == "Time"):
+            found_time = True
+            time = line.split()[3]
+        
+    output_file.write(core_numbers[i] + "  " + time + "\n" )
+    input_file.close()
+    
+output_file.close()
+
+
