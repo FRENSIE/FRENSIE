fb8b5a9814aa6007221019c993ccecffa484c381
Alex Robinson <aprobinson@wisc.edu>
2015-11-03 18:53:46 -0600

    Added additional units that will be required in the other packages.

--- packages/utility/units/src/Utility_BarnUnit.hpp
+++ packages/utility/units/src/Utility_BarnUnit.hpp
@@ -10,19 +10,19 @@
 #define UTILITY_BARN_UNIT_HPP
 
 // Boost Includes
-#include <boost/units/system/cgs/area.hpp>
-#include <boost/units/system/si/energy.hpp>
-#include <boost/units/physical_dimensions/energy.hpp>
-#include <boost/units/base_unit.hpp>
-#include <boost/units/conversion.hpp>
+#include <boost/units/systems/cgs/area.hpp>
 #include <boost/units/make_scaled_unit.hpp>
 #include <boost/units/io.hpp>
 
 namespace Utility{
 
-typedef boost::units::make_scaled_unit<boost::units::cgs::area, boost::units::scale<10,boost::units::static_rational<24> > > >::type Barn;
+namespace Units{
 
-BOOST_UNITS_STATIC_CONSTANT( b, Barn );
+typedef boost::units::make_scaled_unit<boost::units::cgs::area, boost::units::scale<10,boost::units::static_rational<-24> > >::type Barn;
+
+BOOST_UNITS_STATIC_CONSTANT( barn, Barn );
+
+} // end Units namespace
 
 } // end Utility namespace
 
@@ -30,12 +30,12 @@ namespace boost{
 
 namespace units{
 
-std::string name_string( const Utility::Barn& )
+std::string name_string( const Utility::Units::Barn& )
 {
   return "Barn";
 }
 
-std::string symbol_string( const cgs::force& )
+std::string symbol_string( const Utility::Units::Barn& )
 {
   return "b";
 }
