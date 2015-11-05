f390ed52b354293378c891b09755f225e7802961
Alex Robinson <aprobinson@wisc.edu>
2015-11-04 19:03:42 -0600

    Added the atomic and me*c momentum units for use with Compton Profiles (for real this time ;) )!

---
+++ packages/utility/units/src/Utility_MomentumUnits.hpp
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   Utility_MomentumUnits.hpp
+//! \author Alex Robinson
+//! \brief  The momentum units
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef UTILITY_MOMENTUM_UNITS_HPP
+#define UTILITY_MOMENTUM_UNITS_HPP
+
+// Boost Includes
+#include <boost/units/systems/si/momentum.hpp>
+#include <boost/units/conversion.hpp>
+
+// FRENSIE Includes
+#include "Utility_AtomicMomentumUnit.hpp"
+#include "Utility_MeCMomentumUnit.hpp"
+
+BOOST_UNITS_DEFINE_CONVERSION_FACTOR( Utility::Units::AtomicMomentumBaseUnit, Utility::Units::MeCMomentumBaseUnit, double, Utility::PhysicalConstants::fine_structure_constant );
+
+BOOST_UNITS_DEFINE_CONVERSION_FACTOR( Utility::Units::MeCMomentumBaseUnit, Utility::Units::AtomicMomentumBaseUnit, double, 1.0/Utility::PhysicalConstants::fine_structure_constant );
+
+#endif // end UTILITY_MOMENTUM_UNITS_HPP
+
+//---------------------------------------------------------------------------//
+// end Utility_MomentumUnitS.hpp
+//---------------------------------------------------------------------------//
