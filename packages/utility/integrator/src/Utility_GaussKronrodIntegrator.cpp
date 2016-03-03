0b23867c5ee3f8f9c8a74397daaaedf230834af8
Luke Kersting <lkersting@wisc.edu>
2016-03-03 09:57:28 -0600

    Finished unit testing the GaussKronrodIntegrator class and removed all unused functions #56

--- packages/utility/integrator/src/Utility_GaussKronrodIntegrator.cpp
+++ packages/utility/integrator/src/Utility_GaussKronrodIntegrator.cpp
@@ -73,145 +73,6 @@ void GaussKronrodIntegrator::rescaleAbsoluteError(
         }
     };
 };
-/*
-// Update the integral results and errors
-inline void GaussKronrodIntegrator::updateIntegral( 
-        Teuchos::Array<double>& bin_lower_limit, 
-        Teuchos::Array<double>& bin_upper_limit, 
-        Teuchos::Array<double>& bin_result, 
-        Teuchos::Array<double>& bin_error,
-        double& lower_limit_1, 
-        double& upper_limit_1,
-        double& area_1, 
-        double& error_1,
-        double& lower_limit_2,
-        double& upper_limit_2, 
-        double& area_2, 
-        double& error_2,
-        int& last,
-        int& bin_with_max_error ) const
-*/
-// Update the integral results and errors
-void GaussKronrodIntegrator::updateIntegral( 
-    Teuchos::Array<double>& bin_lower_limit, 
-    Teuchos::Array<double>& bin_upper_limit, 
-    Teuchos::Array<double>& bin_result, 
-    Teuchos::Array<double>& bin_error,
-    double& lower_limit_1, 
-    double& upper_limit_1,
-    double& area_1, 
-    double& error_1,
-    double& lower_limit_2,
-    double& upper_limit_2, 
-    double& area_2, 
-    double& error_2,
-    int& last,
-    int& bin_with_max_error ) const
-{
-    if ( error_2 <= error_1 )
-    {
-      bin_lower_limit[last] = lower_limit_2;
-      bin_upper_limit[last] = upper_limit_2;
-      bin_error[last] = error_2;
-      bin_result[last] = area_2;
-
-      // Lower limit already set as lower_limit_1
-      bin_upper_limit[bin_with_max_error] = upper_limit_1;
-      bin_error[bin_with_max_error] = error_1;
-      bin_result[bin_with_max_error] = area_1;
-    }
-    else
-    {
-      bin_lower_limit[last] = lower_limit_1;
-      bin_upper_limit[last] = upper_limit_1;
-      bin_error[last] = error_1;
-      bin_result[last] = area_1;
-
-      bin_lower_limit[bin_with_max_error] = lower_limit_2;
-      // Upper limit already set as upper_limit_2
-      bin_error[bin_with_max_error] = error_2;
-      bin_result[bin_with_max_error] = area_2;
-    }
-};
-
-// Sort the bin order from highest to lowest error 
-/*! \details The error list will be correctly sorted with the exception
- *  of two bins: bin_with_larger_error & bin_with_smaller_error.
- *  The bin_with_larger_error is located near the fron of the order list.
- *  The bin_with_smaller_error is at the end of the used list entries.
- */
-void GaussKronrodIntegrator::sortErrorList( 
-        Teuchos::Array<double>& bin_error,
-        Teuchos::Array<double>& bin_order, 
-        double& maximum_bin_error, 
-        int& bin_with_larger_error,
-        int bin_with_smaller_error, 
-        int nr_max ) const
-{
-
-  //  Check whether the list contains more than two error estimates.
-  if ( bin_with_smaller_error < 2 )
-  {
-    bin_order[0] = 0;
-    bin_order[1] = 1;
-  }
-  else
-  {
-    double larger_bin_error = bin_error[bin_with_larger_error];
-    double smaller_bin_error = bin_error[bin_with_smaller_error];
-
-   /*
-    *  This part of the routine is only executed if, due to a
-    *  difficult integrand, subdivision increased the error
-    *  estimate. in the normal case the insert procedure should
-    *  start after the nr_max-th largest error estimate.
-    */
-    while ( nr_max > 0 && larger_bin_error > bin_error[ bin_order[nr_max-1] ] )
-    {
-      bin_order[nr_max] = bin_order[nr_max-1];
-      nr_max--; 
-    }
-
-    /*
-     *  Compute the number of elements in the list to be maintained
-     *  in descending order. This number depends on the number of
-     *  subdivisions still allowed.
-     */
-    int max_bin;
-    if ( (d_subinterval_limit/2+2) < bin_with_smaller_error )
-      max_bin = d_subinterval_limit + 1 - bin_with_smaller_error;
-    else
-      max_bin = bin_with_smaller_error;
-
-    /*
-     *  Insert larger_bin_error by traversing the list top-down, starting
-     *  comparison from the element bin_error[ bin_order[nr_max+1] ].
-     */
-    int large_bin = nr_max+1;
-    while ( large_bin < max_bin && larger_bin_error < bin_error[ bin_order[large_bin] ] )
-    {
-      bin_order[large_bin-1] = bin_order[large_bin];
-      large_bin++;
-    }
-    bin_order[large_bin-1] = bin_with_larger_error;
-
-    //  Insert smaller_bin_error by traversing the list bottom-up.
-    int small_bin = max_bin-1;
-    while ( small_bin > large_bin -2 && 
-            bin_error[ bin_order[small_bin] ] < smaller_bin_error )
-    {
-      bin_order[small_bin+1] = bin_order[small_bin];
-      small_bin--;
-    }
-    bin_order[small_bin+1] = bin_with_smaller_error;
-  }
-
-  // Set bin_with_larger_error to the bin with maximum error.
-  bin_with_larger_error = bin_order[nr_max];
-
-  // Set maximum_bin_error.
-  maximum_bin_error = bin_error[bin_with_larger_error];
-};
 
 // Sort the bin order from highest to lowest error 
 //! \details The error list will be correctly sorted except bin_1 and bin_2
@@ -306,6 +167,7 @@ void GaussKronrodIntegrator::sortBins(
   bin_order.insert( small_bin+1, number_of_intervals );
 };
 
+
 // get the Wynn Epsilon-Algoirithm extrapolated value
 void GaussKronrodIntegrator::getWynnEpsilonAlgorithmExtrapolation( 
         Teuchos::Array<double>& bin_extrapolated_result, 
@@ -396,7 +258,7 @@ void GaussKronrodIntegrator::getWynnEpsilonAlgorithmExtrapolation(
       break;
     }
 
-    double ss = 1.0/delta1 + 1.0/delta2 - 1.0/delta3;
+    double ss = (1.0/delta1 + 1.0/delta2) - 1.0/delta3;
 
     /* Test to detect irregular behavior in the table, and eventually omit a
      * part of the table adjusting the value of number_of_extrapolated_intervals.
@@ -480,6 +342,7 @@ void GaussKronrodIntegrator::getWynnEpsilonAlgorithmExtrapolation(
   return;
 };  
 
+
 // check the roundoff error 
 void GaussKronrodIntegrator::checkRoundoffError( 
                        const BinTraits& bin, 
@@ -512,6 +375,7 @@ void GaussKronrodIntegrator::checkRoundoffError(
                         Utility::IntegratorException,
                         "Roundoff error prevented tolerance from being achieved" );
 };
+
 
 // check the roundoff error 
 void GaussKronrodIntegrator::checkRoundoffError( 
