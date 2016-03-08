e91f99e2346ffb3fae2931d0833d2028acb4be13
Alex Robinson <aprobinson@wisc.edu>
2016-03-07 20:46:04 -0600

    Working on adding facemc root verification tests.

--- tools/facemc/test/tstFacemcWater-293.6K.hpp
+++ tools/facemc/test/tstFacemcWater-293.6K.hpp
@@ -12,115 +12,40 @@
 // Std Lib Includes
 #include <string>
 
-// Teuchos Includes
-#include <Teuchos_VerboseObject.hpp>
-#include <Teuchos_LocalTestingHelpers.hpp>
-
 // FRENSIE Includes
 #include "MonteCarlo_EstimatorHDF5FileHandler.hpp"
-#include "MonteCarlo_PhaseSpaceDimension.hpp"
-#include "Utility_Tuple.hpp"
-#include "Utility_ArrayString.hpp"
-#include "Utility_UnitTestHarnessExtensions.hpp"
 
-//! Test the output of the Water-293.6K simulation
-bool testSimulationResults( const std::string& simulation_hdf5_file_name )
-{
-  Teuchos::FancyOStream& out = 
-    *Teuchos::VerboseObjectBase::getDefaultOStream();
+//! Conduct a serial test
+int conductSerialTest( int argc, 
+                       char** argv,
+                       const std::string& simulation_hdf5_file_name,
+                       const bool surface_estimators_present );
 
-  bool success = true;
+//! Conduct a parallel test
+int conductParallelTest( int argc,
+                         char** argv,
+                         const std::string& simulation_hdf5_file_name,
+                         const bool surface_estimators_present );
 
-  MonteCarlo::EstimatorHDF5FileHandler hdf5_file_handler(
-         simulation_hdf5_file_name,
-	 MonteCarlo::EstimatorHDF5FileHandler::READ_ONLY_ESTIMATOR_HDF5_FILE );
-  
-  // Check the dimension ordering
-  Teuchos::Array<MonteCarlo::PhaseSpaceDimension> dimension_ordering( 1 ),
-    dimension_ordering_result;
+//! Test the output of the H1-2500K simulation
+bool testSimulationResults( const std::string& simulation_hdf5_file_name,
+                            const bool surface_estimators_present );
 
-  dimension_ordering[0] = MonteCarlo::ENERGY_DIMENSION;
-  
-  hdf5_file_handler.getEstimatorDimensionOrdering(
-					       1u, dimension_ordering_result );
+//! Test estimator 1 data
+bool testEstimator1Data( 
+               const MonteCarlo::EstimatorHDF5FileHandler& hdf5_file_handler );
 
-  TEST_EQUALITY_CONST( dimension_ordering.size(), 1 );
-  TEST_EQUALITY_CONST( dimension_ordering[0], MonteCarlo::ENERGY_DIMENSION );
+//! Test estimator 2 data
+bool testEstimator2Data(
+               const MonteCarlo::EstimatorHDF5FileHandler& hdf5_file_handler );
 
-  // Check the energy bins
-  Utility::ArrayString energy_bin_string( "{1e-11, 2l, 19.0}" );
+//! Test estimator 3 data
+bool testEstimator3Data(
+               const MonteCarlo::EstimatorHDF5FileHandler& hdf5_file_handler );
 
-  Teuchos::Array<double> energy_bins, energy_bins_result;
-  energy_bins = energy_bin_string.getConcreteArray<double>();
-    
-  hdf5_file_handler.getEstimatorBinBoundaries<MonteCarlo::ENERGY_DIMENSION>(
-						      1u, energy_bins_result );
-
-  UTILITY_TEST_COMPARE_FLOATING_ARRAYS( energy_bins,
-                                        energy_bins_result,
-                                        1e-15 );
-
-  // Check the raw entity bin data
-  Teuchos::Array<Utility::Pair<double,double> > raw_bin_data( 3 ),
-    raw_bin_data_result;
-  raw_bin_data[0](3545, 3545);
-  raw_bin_data[1](19866, 19882);
-  raw_bin_data[2](9864020, 9866206);
-    
-  hdf5_file_handler.getRawEstimatorEntityBinData( 1u, 1u, raw_bin_data_result);
-    
-  UTILITY_TEST_COMPARE_FLOATING_ARRAYS( raw_bin_data,
-                                        raw_bin_data_result,
-                                        1e-15 );
-
-  // Check the processed entity bin data
-  Teuchos::Array<Utility::Pair<double,double> > processed_bin_data( 3 ),
-    processed_bin_data_result;
-  processed_bin_data[0](0.000354499999999999997, 0.0167924818626849701);
-  processed_bin_data[1](0.00198659999999999982, 0.00709068410485995219);
-  processed_bin_data[2](0.986402000000000001, 3.74300990581649494e-05);
-  
-  hdf5_file_handler.getProcessedEstimatorEntityBinData(
-					   1u, 1u, processed_bin_data_result );
-    
-  UTILITY_TEST_COMPARE_FLOATING_ARRAYS( processed_bin_data,
-                                        processed_bin_data_result,
-                                        1e-12 );
-
-  // Check the processed entity bin data
-  Teuchos::Array<Utility::Quad<double,double,double,double> >
-    raw_total_data( 1 ), raw_total_data_result;
-  raw_total_data[0]( 9887431, 9890059, 9895315, 9905827 );
-    
-  hdf5_file_handler.getRawEstimatorTotalData( 1u, raw_total_data_result );
-  
-  UTILITY_TEST_COMPARE_FLOATING_ARRAYS( raw_total_data,
-                                        raw_total_data_result,
-                                        1e-15 );
-
-  // Check the processed total data (ignore fom)
-  Teuchos::Array<Utility::Quad<double,double,double,double> >
-    processed_total_data( 1 ), processed_total_data_result;
-  processed_total_data[0]( 0.9887431, 
-                           3.41377775187904e-05, 
-                           8.39116752915233e-06, 
-                           0.0 );
-    
-  hdf5_file_handler.getProcessedEstimatorTotalData(
-					     1u, processed_total_data_result );
-
-  TEST_FLOATING_EQUALITY( processed_total_data[0].first,
-                          processed_total_data_result[0].first,
-                          1e-15 );
-  TEST_FLOATING_EQUALITY( processed_total_data[0].second,
-                          processed_total_data_result[0].second,
-                          1e-15 );
-  TEST_FLOATING_EQUALITY( processed_total_data[0].third,
-                          processed_total_data_result[0].third,
-                          1e-15 );    
-
-  return success;
-}
+//! Test estimator 4 data
+bool testEstimator4Data(
+               const MonteCarlo::EstimatorHDF5FileHandler& hdf5_file_handler );
 
 #endif // end TST_FACEMC_WATER_293K_HPP
 
--- tools/facemc/test/water-293.6K/water_sphere.c
+++ tools/facemc/test/water-293.6K/water_sphere.c
@@ -1,21 +1,21 @@
 //---------------------------------------------------------------------------//
 //!
-//! \file   h_sphere.c
+//! \file   water_sphere.c
 //! \author Alex Robinson
-//! \brief  Geometry for Neutron H-1 sphere at 2500K verification problem
+//! \brief  Geometry for Neutron water sphere at 293.6K verification problem
 //!
 //---------------------------------------------------------------------------//
 
-void h_sphere()
+void water_sphere()
 {
   // Set up manager of the geometry world
   gSystem->Load( "libGeom" );
   
   TGeoManager* geom = new TGeoManager( 
-                   "h_sphere",
-                   "Geometry for the neutron H-1 sphere at 2500K test prob.");
+                 "water_sphere",
+                 "Geometry for the neutron water sphere at 293.6K test prob.");
   
-  // Create the hydrogen material
+  // Create the water material
   TGeoMaterial* mat_1 = new TGeoMaterial( "mat_1", 1, 1, 1 );
   TGeoMedium* med_1 = new TGeoMedium( "med_1", 2, mat_1 );
 
@@ -27,26 +27,26 @@ void h_sphere()
   TGeoMaterial* graveyard_mat = new TGeoMaterial( "graveyard", 0, 0, 0 );
   TGeoMedium* graveyard_med = new TGeoMedium( "graveyard", 3, graveyard_mat );
 
-  // Create the hydrogen volume
-  TGeoVolume* h_sphere_volume = 
-    geom->MakeSphere( "SPHERE", med_1, 0.0, 1.0 );
+  // Create the water volume
+  TGeoVolume* water_sphere_volume = 
+    geom->MakeSphere( "SPHERE", med_1, 0.0, 3.0 );
   
-  h_sphere_volume->SetUniqueID( 1 );
+  water_sphere_volume->SetUniqueID( 1 );
 
   // Create the void volume
   TGeoVolume* void_cube_volume = geom->MakeBox( 
-                                             "CUBE", void_med, 2.5, 2.5, 2.5 );
+                                             "CUBE", void_med, 6.5, 6.5, 6.5 );
   
   void_cube_volume->SetUniqueID( 2 );
 
   // Create the graveyard volume
   TGeoVolume* graveyard_volume = geom->MakeBox( 
-                                   "GRAVEYARD", graveyard_med, 3.0, 3.0, 3.0 );
+                                   "GRAVEYARD", graveyard_med, 7.0, 7.0, 7.0 );
 
   graveyard_volume->SetUniqueID( 3 );
 
-  // Place the hydrogen sphere inside of the void cube
-  void_cube_volume->AddNode( h_sphere_volume, 1 );
+  // Place the water sphere inside of the void cube
+  void_cube_volume->AddNode( water_sphere_volume, 1 );
 
   // Place the void cube inside of the graveyard
   graveyard_volume->AddNode( void_cube_volume, 1 );
@@ -60,12 +60,12 @@ void h_sphere()
   // graveyard_volume->Draw();
 
   // Export the geometry
-  geom->Export( "h_sphere.root" );
+  geom->Export( "water_sphere.root" );
   
   // Finished
   exit(0);
 }
 
 //---------------------------------------------------------------------------//
-// end h_sphere.c
+// end water_sphere.c
 //---------------------------------------------------------------------------//
