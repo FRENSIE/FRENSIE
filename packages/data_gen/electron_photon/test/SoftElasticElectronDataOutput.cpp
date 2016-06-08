51304e7d2681be3f3483c8acb69304a65a135f9a
Luke Kersting <lkersting@wisc.edu>
2016-06-08 09:40:16 -0600

    FIXWS_SAVE_INDEX

---
+++ packages/data_gen/electron_photon/test/SoftElasticElectronDataOutput.cpp
@@ -0,0 +1,334 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   SoftElasticElectronDataOutput.cpp
+//! \author Luke Kersting
+//! \brief  Soft elastic electron data output
+//!
+//---------------------------------------------------------------------------//
+
+// Std Lib Includes
+#include <iostream>
+
+// Trilinos Includes
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_VerboseObject.hpp>
+#include <Teuchos_RCP.hpp>
+
+// FRENSIE Includes
+#include "DataGen_ElasticElectronMomentsEvaluator.hpp"
+#include "Data_ElectronPhotonRelaxationDataContainer.hpp"
+#include "Utility_UnitTestHarnessExtensions.hpp"
+#include "Utility_SloanRadauQuadrature.hpp"
+#include "MonteCarlo_AnalogElasticElectroatomicReaction.hpp"
+#include "MonteCarlo_AnalogElasticElectronScatteringDistribution.hpp"
+#include "MonteCarlo_ElasticElectronScatteringDistributionNativeFactory.hpp"
+#include "DataGen_StandardSoftElasticElectronDataGenerator.hpp"
+
+//---------------------------------------------------------------------------//
+// Testing Structs.
+//---------------------------------------------------------------------------//
+class TestElasticElectronMomentsEvaluator : public DataGen::ElasticElectronMomentsEvaluator
+{
+public:
+  TestElasticElectronMomentsEvaluator(
+        const Data::ElectronPhotonRelaxationDataContainer& data_container )
+    : ElasticElectronMomentsEvaluator( data_container, 0.999999 )
+  { /* ... */ }
+
+  ~TestElasticElectronMomentsEvaluator()
+  { /* ... */ }
+
+  // Allow public access to the ElasticElectronMomentsEvaluator protected member functions
+  using DataGen::ElasticElectronMomentsEvaluator::evaluateCutoffPDFMoment;
+  using DataGen::ElasticElectronMomentsEvaluator::evaluateScreenedRutherfordPDFMoment;
+  using DataGen::ElasticElectronMomentsEvaluator::evaluateScreenedRutherfordPDFMomentByRecursion;
+  using DataGen::ElasticElectronMomentsEvaluator::evaluateScreenedRutherfordPDFMomentByNumericalIntegration;
+};
+
+//---------------------------------------------------------------------------//
+// Testing Variables
+//---------------------------------------------------------------------------//
+
+Teuchos::RCP<Data::ElectronPhotonRelaxationDataContainer> data;
+Teuchos::RCP<DataGen::ElasticElectronMomentsEvaluator> evaluator;
+Teuchos::RCP<TestElasticElectronMomentsEvaluator> test_evaluator;
+Teuchos::RCP<const DataGen::StandardSoftElasticElectronDataGenerator> data_generator;
+Teuchos::RCP<const MonteCarlo::ScreenedRutherfordElasticElectronScatteringDistribution>
+    rutherford_distribution;
+Teuchos::RCP<const MonteCarlo::CutoffElasticElectronScatteringDistribution>
+    cutoff_distribution;
+
+//---------------------------------------------------------------------------//
+// Testing Functions.
+//---------------------------------------------------------------------------//
+bool notEqualZero( double value )
+{
+  return value != 0.0;
+}
+
+//---------------------------------------------------------------------------//
+// Custom main function
+//---------------------------------------------------------------------------//
+int main( int argc, char** argv )
+{
+  std::string test_native_file_name;
+
+  Teuchos::CommandLineProcessor& clp = Teuchos::UnitTestRepository::getCLP();
+
+  clp.setOption( "test_native_file",
+		 &test_native_file_name,
+		 "Test Native file name" );
+
+  const Teuchos::RCP<Teuchos::FancyOStream> out =
+    Teuchos::VerboseObjectBase::getDefaultOStream();
+
+  Teuchos::CommandLineProcessor::EParseCommandLineReturn parse_return =
+    clp.parse(argc,argv);
+
+  if ( parse_return != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL ) {
+    *out << "\nEnd Result: TEST FAILED" << std::endl;
+    return parse_return;
+  }
+
+  {
+  // Create the native data file container
+  data.reset( new Data::ElectronPhotonRelaxationDataContainer(
+						     test_native_file_name ) );
+
+
+  // Create the hard elastic distributions ( both Cutoff and Screened Rutherford )
+  MonteCarlo::ElasticElectronScatteringDistributionNativeFactory::createHardElasticDistributions(
+    cutoff_distribution,
+    rutherford_distribution,
+    *data,
+    0.999999 );
+
+  // Create the moment evaluator
+  evaluator.reset( new DataGen::ElasticElectronMomentsEvaluator(
+                                    *data ) );
+
+  // Create the test moment evaluator
+  test_evaluator.reset( new TestElasticElectronMomentsEvaluator(
+                                    *data ) );
+
+  data_generator.reset(
+		   new DataGen::StandardSoftElasticElectronDataGenerator(
+				     data->getAtomicNumber(),
+				     data,
+				     0.00001,
+				     20.0,
+				     0.999999 ) );
+  }
+
+  std::vector<double> angular_energy_grid =
+    data->getElasticAngularEnergyGrid();
+
+  std::vector<double> eta(angular_energy_grid.size());
+  eta[0] = 1.189646992674E+01L;
+  eta[1] = 3.629783667879E-02L;
+  eta[2] = 1.679268560571E-02L;
+  eta[3] = 7.908970260881E-03L;
+  eta[4] = 3.774166375312E-03L;
+  eta[5] = 1.815305453727E-03L;
+  eta[6] = 8.743857991990E-04L;
+  eta[7] = 4.179170609007E-04L;
+  eta[8] = 1.953805580523E-04L;
+  eta[9] = 8.739160865170E-05L;
+  eta[10] = 2.589467963149E-07L;
+  eta[11] = 6.032430519875E-08L;
+  eta[12] = 2.620863475107E-08L;
+  eta[13] = 1.457796275283E-08L;
+  eta[14] = 6.407445779740E-09L;
+  eta[15] = 2.855935652488E-15L;
+
+  std::vector<Utility::long_float> moments(3);
+  moments[0] = Utility::long_float(1);
+
+  std::ofstream myfile;
+  myfile.open ("soft_elastic_data.txt");
+  myfile << "Screened Rutherford moments calculated using the recursion relationship and ITS eta.\n";
+  myfile << "Element Z = \t"<< data->getAtomicNumber() << "\n";
+  myfile << "\tEnergy" << std::setw(20)
+         << "\t\tEta" << std::setw(20)
+         << "\t\tMoment 1" << std::setw(20)
+         << "\t\tMoment 2" << std::setw(20)
+         << "\t\tMu \n";
+
+  for ( unsigned i = 0; i < angular_energy_grid.size(); i++ )
+  {
+
+  // Evaluate the 1st and 2nd moment
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByRecursion(
+        moments[1],
+        eta[i]*Utility::long_float(2),
+        1 );
+
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByRecursion(
+        moments[2],
+        eta[i]*Utility::long_float(2),
+        2 );
+
+  moments[1] /= moments[0];
+  moments[2] /= moments[0];
+
+  // Use radau quadrature to find the discrete angles and weights from the moments
+  Teuchos::RCP<Utility::SloanRadauQuadrature> radau_quadrature(
+      new Utility::SloanRadauQuadrature( moments ) );
+
+  std::vector<double> discrete_angles, weights;
+  radau_quadrature->getRadauNodesAndWeights( discrete_angles,
+                                             weights,
+                                             2 );
+
+    myfile << std::setprecision(15) << std::scientific
+           << angular_energy_grid[i] << "\t"
+           << eta[i] << "\t"
+           << moments[1] << "\t"
+           << moments[2] << "\t"
+           << discrete_angles[0] << "\n";
+  }
+
+  myfile << "\n\nScreened Rutherford moments calculated using numerical integration and ITS eta.\n";
+  myfile << "Element Z = \t"<< data->getAtomicNumber() << "\n";
+  myfile << "\tEnergy" << std::setw(20)
+         << "\t\tEta" << std::setw(20)
+         << "\t\tMoment 1" << std::setw(20)
+         << "\t\tMoment 2" << std::setw(20)
+         << "\t\tMu \n";
+
+  for ( unsigned i = 0; i < angular_energy_grid.size(); i++ )
+  {
+
+  // Evaluate the 1st and 2nd moment
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByNumericalIntegration(
+        moments[1],
+        eta[i]*Utility::long_float(2),
+        angular_energy_grid[i],
+        1 );
+
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByNumericalIntegration(
+        moments[2],
+        eta[i]*Utility::long_float(2),
+        angular_energy_grid[i],
+        2 );
+
+  moments[1] /= moments[0];
+  moments[2] /= moments[0];
+
+  // Use radau quadrature to find the discrete angles and weights from the moments
+  Teuchos::RCP<Utility::SloanRadauQuadrature> radau_quadrature(
+      new Utility::SloanRadauQuadrature( moments ) );
+
+  std::vector<double> discrete_angles, weights;
+  radau_quadrature->getRadauNodesAndWeights( discrete_angles,
+                                             weights,
+                                             2 );
+
+    myfile << std::setprecision(15) << std::scientific
+           << angular_energy_grid[i] << "\t"
+           << eta[i] << "\t"
+           << moments[1] << "\t"
+           << moments[2] << "\t"
+           << discrete_angles[0] << "\n";
+  }
+
+
+  myfile << "\n\nScreened Rutherford moments calculated using the recursion relationship and FRENSIE eta.\n";
+  myfile << "Element Z = \t"<< data->getAtomicNumber() << "\n";
+  myfile << "\tEnergy" << std::setw(20)
+         << "\t\tEta" << std::setw(20)
+         << "\t\tMoment 1" << std::setw(20)
+         << "\t\tMoment 2" << std::setw(20)
+         << "\t\tMu \n";
+
+  for ( unsigned i = 0; i < angular_energy_grid.size(); i++ )
+  {
+
+  // Calcuate Moliere's modified screening constant (eta)
+  eta[i] =
+    rutherford_distribution->evaluateMoliereScreeningConstant( angular_energy_grid[i] );
+
+  // Evaluate the 1st and 2nd moment
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByRecursion(
+        moments[1],
+        eta[i],
+        1 );
+
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByRecursion(
+        moments[2],
+        eta[i],
+        2 );
+/*
+  moments[1] /= moments[0];
+  moments[2] /= moments[0];
+*/
+  // Use radau quadrature to find the discrete angles and weights from the moments
+  Teuchos::RCP<Utility::SloanRadauQuadrature> radau_quadrature(
+      new Utility::SloanRadauQuadrature( moments ) );
+
+  std::vector<double> discrete_angles, weights;
+  radau_quadrature->getRadauNodesAndWeights( discrete_angles,
+                                             weights,
+                                             2 );
+
+    myfile << std::setprecision(15) << std::scientific
+           << angular_energy_grid[i] << "\t"
+           << eta[i]/Utility::long_float(2) << "\t"
+           << moments[1] << "\t"
+           << moments[2] << "\t"
+           << discrete_angles[0] << "\n";
+  }
+
+  myfile << "\n\nScreened Rutherford moments calculated using numerical integration and FRENSIE eta.\n";
+  myfile << "Element Z = \t"<< data->getAtomicNumber() << "\n";
+  myfile << "\tEnergy" << std::setw(20)
+         << "\t\tEta" << std::setw(20)
+         << "\t\tMoment 1" << std::setw(20)
+         << "\t\tMoment 2" << std::setw(20)
+         << "\t\tMu \n";
+
+  for ( unsigned i = 0; i < angular_energy_grid.size(); i++ )
+  {
+
+  // Evaluate the 1st and 2nd moment
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByNumericalIntegration(
+        moments[1],
+        eta[i],
+        angular_energy_grid[i],
+        1 );
+
+  test_evaluator->evaluateScreenedRutherfordPDFMomentByNumericalIntegration(
+        moments[2],
+        eta[i],
+        angular_energy_grid[i],
+        2 );
+/*
+  moments[1] /= moments[0];
+  moments[2] /= moments[0];
+*/
+  // Use radau quadrature to find the discrete angles and weights from the moments
+  Teuchos::RCP<Utility::SloanRadauQuadrature> radau_quadrature(
+      new Utility::SloanRadauQuadrature( moments ) );
+
+  std::vector<double> discrete_angles, weights;
+  radau_quadrature->getRadauNodesAndWeights( discrete_angles,
+                                             weights,
+                                             2 );
+
+    myfile << std::setprecision(15) << std::scientific
+           << angular_energy_grid[i] << "\t"
+           << eta[i]/Utility::long_float(2) << "\t"
+           << moments[1] << "\t"
+           << moments[2] << "\t"
+           << discrete_angles[0] << "\n";
+  }
+
+  myfile.close();
+
+
+  return 0;
+}
+
+//---------------------------------------------------------------------------//
+// end tstElasticElectronMomentsEvaluator.cpp
+//---------------------------------------------------------------------------//
