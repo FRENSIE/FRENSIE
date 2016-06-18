d47808616ece4cb0b9333e48e0e0ba538e525b2f
AJ <ajgross2@wisc.edu>
2016-06-18 16:55:50 -0500

    Fixed .sh file to properly run the root geometry

--- examples/DagMC_verification/Concrete_Slab_Albeto/concrete_run.sh
+++ examples/DagMC_verification/Concrete_Slab_Albeto/concrete_run.sh
@@ -6,7 +6,7 @@
 mcnp5 i=concrete.i n=concrete_results_ tasks 12
 
 ## Run facemc using root geometry
-##.././facemc --sim_info=concrete_hist.xml --geom_def=concrete_geom_root.xml --mat_def=concrete_mat.xml --resp_def=concrete_response.xml --est_def=concrete_est.xml --src_def=concrete_src.xml --cross_sec_dir=/home/software/mcnpdata --threads=12
+.././facemc --sim_info=concrete_hist.xml --geom_def=concrete_geom_root.xml --mat_def=concrete_mat.xml --resp_def=concrete_response.xml --est_def=concrete_est_root.xml --src_def=concrete_src.xml --cross_sec_dir=/home/software/mcnpdata --threads=12
 
 ## Run facemc using Cubit geometry
 .././facemc --sim_info=concrete_hist.xml --geom_def=concrete_geom_cub.xml --mat_def=concrete_mat.xml --resp_def=concrete_response.xml --est_def=concrete_est.xml --src_def=concrete_src.xml --cross_sec_dir=/home/software/mcnpdata --threads=12
