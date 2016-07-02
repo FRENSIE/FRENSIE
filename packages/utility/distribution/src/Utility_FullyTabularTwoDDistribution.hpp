f5e30d5447b1a6846bfcebdffee380493e55cc8e
Alex Robinson <aprobinson@wisc.edu>
2016-07-02 14:59:41 -0500

    Working on implementing the new TwoDDistribution classes.

---
+++ packages/utility/distribution/src/Utility_FullyTabularTwoDDistribution.hpp
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   Utility_FullyTabularTwoDDistribution.hpp
+//! \author Alex Robinson
+//! \brief  The fully tabular two-dimensional distribution class declaration
+//!
+//---------------------------------------------------------------------------//
+
+
+
+//---------------------------------------------------------------------------//
+// end Utility_FullyTabularTwoDDistribution.hpp
+//---------------------------------------------------------------------------//
---
+++ packages/utility/distribution/src/Utility_TabularTwoDDistribution.hpp
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   Utility_TabularTwoDDistribution.hpp
+//! \author Alex Robinson
+//! \brief  The tabular two-dimensional distribution class declaration
+//!
+//---------------------------------------------------------------------------//
+
+
+
+//---------------------------------------------------------------------------//
+// end Utility_TabularTwoDimensionalDistribution.hpp
+//---------------------------------------------------------------------------//
