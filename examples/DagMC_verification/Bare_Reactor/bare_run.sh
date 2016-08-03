1d73e36a2fcecb308e53e3d58b392e31ff3e7371
ajgross2 <ajgross2@wisc.edu>
2016-08-03 13:17:22 -0500

    Made some test run edits

--- examples/DagMC_verification/Bare_Reactor/bare_run.sh
+++ examples/DagMC_verification/Bare_Reactor/bare_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## chmod +x *.sh
 
 ## Validation runs comparing 
 
@@ -11,8 +12,8 @@
 ## Run facemc using Cubit geometry
 #.././facemc --sim_info=bare_hist.xml --geom_def=bare_geom_cub.xml --mat_def=bare_mat.xml --resp_def=bare_response.xml --est_def=bare_est.xml --src_def=bare_src.xml --cross_sec_dir=/home/software/mcnpdata --simulation_name=bare_dag.h5 --threads=12
 
-#python ~/software/frensie/bin/edump.py -f bare_root.h5 -e 4 -i total -b Energy > root_tl.out
-#python ~/software/frensie/bin/edump.py -f bare_dag.h5 -e 4 -i total -b Energy > dag_tl.out
-#python ~/software/frensie/bin/data_combiner.py -m mcnp_tl.out -f root_tl.out -o combined_tl_root.out
-#python ~/software/frensie/bin/data_combiner.py -m mcnp_tl.out -f dag_tl.out -o combined_tl_dag.out
+python ~/software/frensie/bin/edump.py -f bare_root.h5 -e 4 -i total -b Energy > root_tl.out
+python ~/software/frensie/bin/edump.py -f bare_dag.h5 -e 4 -i total -b Energy > dag_tl.out
+python ~/software/frensie/bin/data_combiner.py -m mcnp_tl.out -f root_tl.out -o combined_tl_root.out
+python ~/software/frensie/bin/data_combiner.py -m mcnp_tl.out -f dag_tl.out -o combined_tl_dag.out
 gnuplot plot_agreement.p
