7d3866d3dde74dab1870872db4cd04a746f8dcca
Luke Kersting <lkersting@wisc.edu>
2016-08-10 10:10:45 -0600

    Added an additional function the the Brem scat dist Native factory class that takes the brem energy grid as an argument instead of using the energy grid from the native data file. This function is for generating adjoint data which requires manipulating the brem energy grid. #5

--- packages/monte_carlo/collision/native/src/MonteCarlo_AtomicReaction.hpp
+++ packages/monte_carlo/collision/native/src/MonteCarlo_AtomicReaction.hpp
@@ -42,7 +42,7 @@ public:
 
   //! Return the cross section at the given energy (efficient)
   virtual double getCrossSection( const double energy,
-				  const unsigned bin_index ) const = 0;
+                                  const unsigned bin_index ) const = 0;
 
   //! Return the threshold energy
   virtual double getThresholdEnergy() const = 0;
