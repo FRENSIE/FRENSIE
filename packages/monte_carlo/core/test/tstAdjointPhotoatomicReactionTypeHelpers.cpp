558c7c62b595c398971419f0febeb0a811929543
Alex Robinson <aprobinson@wisc.edu>
2016-10-03 16:18:38 -0500

    Added tests for the PhotoatomicReactionType helpers.

--- packages/monte_carlo/core/test/tstAdjointPhotoatomicReactionTypeHelpers.cpp
+++ packages/monte_carlo/core/test/tstAdjointPhotoatomicReactionTypeHelpers.cpp
@@ -449,5 +449,318 @@ TEUCHOS_UNIT_TEST( AdjointPhotoatomicReactionType,
 }
 
 //---------------------------------------------------------------------------//
+// Check that an adjoint photoatomic reaction type can be placed in an
+// output stream
+TEUCHOS_UNIT_TEST( AdjointPhotoatomicReactionType, ostream_operator )
+{
+  std::ostringstream oss;
+
+  oss << MonteCarlo::TOTAL_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Total Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+
+  oss << MonteCarlo::TOTAL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Total Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+
+  oss << MonteCarlo::K_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "K Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+ 
+  oss << MonteCarlo::L1_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "L1 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::L2_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "L2 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::L3_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "L3 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::M1_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "M1 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::M2_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "M2 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::M3_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "M3 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::M4_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "M4 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::M5_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "M5 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N1_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N1 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N2_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N2 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N3_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N3 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N4_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N4 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N5_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N5 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N6_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N6 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::N7_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "N7 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O1_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O1 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O2_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O2 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O3_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O3 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O4_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O4 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O5_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O5 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O6_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O6 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O7_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O7 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O8_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O8 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::O9_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "O9 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P1_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P1 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P2_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P2 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P3_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P3 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P4_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P4 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P5_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P5 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P6_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P6 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P7_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P7 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P8_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P8 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P9_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P9 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P10_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P10 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::P11_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "P11 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::Q1_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Q1 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::Q2_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Q2 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::Q3_SUBSHELL_INCOHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Q3 Subshell Incoherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::COHERENT_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Coherent Adjoint Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::PAIR_PRODUCTION_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Pair Production Photoatomic Reaction" );
+
+  oss.str( "" );
+  oss.clear();
+  
+  oss << MonteCarlo::TRIPLET_PRODUCTION_ADJOINT_PHOTOATOMIC_REACTION;
+
+  TEST_EQUALITY_CONST( oss.str(), "Triplet Production Photoatomic Reaction" );
+}
+  
+//---------------------------------------------------------------------------//
 // end tstAdjointPhotoatomicReactionTypeHelpers.cpp
 //---------------------------------------------------------------------------//
