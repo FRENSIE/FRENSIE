61235be504057afd0b4c57af098571ecbd132862
Alex Robinson <aprobinson@wisc.edu>
2016-10-10 18:12:00 -0500

    Finished the LineEnergyAdjointPhotoatomicReaction class and the unit tests for it.

---
+++ packages/monte_carlo/collision/native/src/MonteCarlo_LineEnergyAdjointPhotoatomicReaction.cpp
@@ -0,0 +1,162 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   MonteCarlo_LineEnergyAdjontPhotoatomicReaction.hpp
+//! \author Alex Robinson
+//! \brief  The line energy adjoint photoatomic reaction class def.
+//!
+//---------------------------------------------------------------------------//
+
+// Trilinos Includes
+#include <Teuchos_RCP.hpp>
+
+// FRENSIE Includes
+#include "MonteCarlo_LineEnergyAdjointPhotoatomicReaction.hpp"
+#include "Utility_ContractException.hpp"
+
+namespace MonteCarlo{
+
+// Constructor
+LineEnergyAdjointPhotoatomicReaction::LineEnergyAdjointPhotoatomicReaction(
+    const AdjointPhotoatomicReactionType reaction,
+    const double cross_section_value,
+    const std::shared_ptr<const LineEnergyAdjointPhotonScatteringDistribution>&
+    energy_dist )
+  : d_reaction_type( reaction ),
+    d_cross_section( cross_section_value ),
+    d_scattering_distribution( energy_dist )
+{
+  // Make sure the cross section is valid
+  testPrecondition( cross_section_value > 0.0 );
+  // Make sure the energy distribution is valid
+  testPrecondition( energy_dist.get() != NULL );
+}
+
+// Return the minimum outgoing energy
+double LineEnergyAdjointPhotoatomicReaction::getMinOutgoingEnergy() const
+{
+  return d_scattering_distribution->getMinOutgoingEnergy();
+}
+
+// Return the maximum outgoing energy
+double LineEnergyAdjointPhotoatomicReaction::getMaxOutgoingEnergy() const
+{
+  return d_scattering_distribution->getMaxOutgoingEnergy();
+}
+
+// Test if the energy falls within the energy grid
+bool LineEnergyAdjointPhotoatomicReaction::isEnergyWithinEnergyGrid(
+                                                    const double energy ) const
+{
+  return energy == d_scattering_distribution->getLineEnergy();
+}
+
+// Return the cross section at the given energy
+double LineEnergyAdjointPhotoatomicReaction::getCrossSection(
+                                                    const double energy ) const
+{
+  if( energy == d_scattering_distribution->getLineEnergy() )
+    return d_cross_section;
+  else
+    return 0.0;
+}
+
+// Return the cross section at the given energy (efficient)
+double LineEnergyAdjointPhotoatomicReaction::getCrossSection(
+                                               const double energy,
+                                               const unsigned bin_index ) const
+{
+  return this->getCrossSection( energy );    
+}
+
+// Return the line energy
+double LineEnergyAdjointPhotoatomicReaction::getLineEnergy() const
+{
+  return d_scattering_distribution->getLineEnergy();
+}
+
+// Return the max energy
+double LineEnergyAdjointPhotoatomicReaction::getMaxEnergy() const
+{
+  return d_scattering_distribution->getLineEnergy();
+}
+
+// Return the threshold energy
+double LineEnergyAdjointPhotoatomicReaction::getThresholdEnergy() const
+{
+  return d_scattering_distribution->getLineEnergy();
+}
+
+// Return the number of adjoint photons emitted from the rxn at the given energy
+unsigned LineEnergyAdjointPhotoatomicReaction::getNumberOfEmittedPhotons(
+                                                    const double energy ) const
+{
+  if( energy == d_scattering_distribution->getLineEnergy() )
+    return 1u;
+  else
+    return 0u;
+}
+
+// Return the number of adjoint electrons emitted from the rxn at the given energy
+unsigned LineEnergyAdjointPhotoatomicReaction::getNumberOfEmittedElectrons(
+                                                    const double energy ) const
+{
+  return 0u;
+}
+
+// Return the reaction type
+AdjointPhotoatomicReactionType LineEnergyAdjointPhotoatomicReaction::getReactionType() const
+{
+  return d_reaction_type;
+}
+
+// Simulate the reaction
+/*! \details Only probe particles should ever enter this react method as they
+ * are the only particle types that will ever have exactly the line energy
+ * where this reaction is defined.
+ */
+void LineEnergyAdjointPhotoatomicReaction::react(
+                               AdjointPhotonState& adjoint_photon,
+                               ParticleBank& bank,
+                               Data::SubshellType& shell_of_interaction ) const
+{
+  // Make sure only probe states enter this react method
+  testPrecondition( adjoint_photon.isProbe() );
+  // Make sure the energy is valid
+  testPrecondition( adjoint_photon.getEnergy() ==
+                    d_scattering_distribution->getLineEnergy() );
+
+  if( adjoint_photon.getEnergy() ==
+      d_scattering_distribution->getLineEnergy() )
+  {
+    // Create a clone of the probe particle
+    Teuchos::RCP<AdjointPhotonState> adjoint_photon_clone(
+                      new AdjointPhotonState( adjoint_photon, false, false ) );
+
+    // Scatter the clone
+    d_scattering_distribution->scatterAdjointPhoton( *adjoint_photon_clone,
+                                                     bank,
+                                                     shell_of_interaction );
+
+    adjoint_photon_clone->incrementCollisionNumber();
+    
+    // Add the clone to the bank
+    bank.push( adjoint_photon_clone );
+    
+    // Kill the probe
+    adjoint_photon.setAsGone();
+  }
+}
+
+// Return the head of the energy grid
+/*! \details There is no energy grid with line energy reactions.
+ */
+const double* LineEnergyAdjointPhotoatomicReaction::getEnergyGridHead() const
+{
+  return NULL;
+}
+  
+} // end MonteCarlo namespace
+
+//---------------------------------------------------------------------------//
+// end MonteCarlo_LineEnergyAdjointPhotoatomicReaction.cpp
+//---------------------------------------------------------------------------//
