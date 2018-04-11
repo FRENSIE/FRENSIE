57927d3db82aec486f542c5a65589ad88fbaada6
Alex Robinson <aprobinson@wisc.edu>
2018-04-11 17:16:56 -0500

    Finished updating the PhotonuclearNeutronScatteringDistributionACEFactory, PhotonuclearScatteringDistributionACEFactory and the StandardPhotonuclearReaction using the FRENSIE unit test harness.

--- packages/monte_carlo/collision/photon/test/tstPhotonuclearScatteringDistributionACEFactory.cpp
+++ packages/monte_carlo/collision/photon/test/tstPhotonuclearScatteringDistributionACEFactory.cpp
@@ -10,16 +10,12 @@
 #include <string>
 #include <iostream>
 
-// Trilinos Includes
-#include <Teuchos_UnitTestHarness.hpp>
-#include <Teuchos_RCP.hpp>
-
 // FRENSIE Includes
 #include "MonteCarlo_PhotonuclearScatteringDistributionACEFactory.hpp"
-#include "MonteCarlo_NuclearReactionType.hpp"
+#include "MonteCarlo_PhotonuclearReactionType.hpp"
 #include "Data_ACEFileHandler.hpp"
 #include "Data_XSSPhotonuclearDataExtractor.hpp"
-#include "MonteCarlo_PhotonuclearReactionType.hpp"
+#include "Utility_UnitTestHarnessWithMain.hpp"
 
 //---------------------------------------------------------------------------//
 // Testing Structs.
@@ -30,8 +26,9 @@ public:
   TestPhotonuclearScatteringDistributionACEFactory(
                          const std::string& table_name,
 			 const double atomic_weight_ratio,
-			 const Data::XSSPhotonuclearDataExtractor raw_nuclide_data )
-    : MonteCarlo::PhotonuclearScatteringDistributionACEFactory( table_name,
+		   const Data::XSSPhotonuclearDataExtractor& raw_nuclide_data )
+    : MonteCarlo::PhotonuclearScatteringDistributionACEFactory(
+                                                           table_name,
                                                            atomic_weight_ratio,
 							   raw_nuclide_data )
   { /* ... */ }
@@ -48,128 +45,169 @@ public:
   using MonteCarlo::PhotonuclearScatteringDistributionACEFactory::getReactionEnergyDist;
   using MonteCarlo::PhotonuclearScatteringDistributionACEFactory::getReactionEnergyDistStartIndex;
 };
+
 //---------------------------------------------------------------------------//
 // Testing Variables.
 //---------------------------------------------------------------------------//
-std::string test_basic_c12_ace_file_name;
-std::string test_basic_c12_ace_table_name;
 
-Teuchos::RCP<Data::ACEFileHandler> ace_file_handler_c12;
-Teuchos::RCP<Data::XSSPhotonuclearDataExtractor> xss_data_extractor_c12;
-Teuchos::RCP<TestPhotonuclearScatteringDistributionACEFactory> photonuclear_distribution_factory_c12;
+std::unique_ptr<const Data::XSSPhotonuclearDataExtractor> xss_data_extractor;
+std::unique_ptr<const TestPhotonuclearScatteringDistributionACEFactory> photonuclear_distribution_factory_c12;
 
 //---------------------------------------------------------------------------//
 // Tests.
 //---------------------------------------------------------------------------//
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionOrdering_c12 )
+// Check that the reaction ordering has been extracted correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionOrdering )
 {
-  ace_file_handler_c12.reset(new Data::ACEFileHandler( test_basic_c12_ace_file_name,
-                                                   test_basic_c12_ace_table_name,
-                                                   1u ) );
+  auto reaction_ordering =
+    photonuclear_distribution_factory_c12->getReactionOrdering();
 
-  xss_data_extractor_c12.reset(
-   new Data::XSSPhotonuclearDataExtractor( ace_file_handler_c12->getTableNXSArray(),
-                                      ace_file_handler_c12->getTableJXSArray(),
-                                      ace_file_handler_c12->getTableXSSArray()));
+  // Only 1 reaction produces an outgoing photon, reaction 5
+  FRENSIE_REQUIRE_EQUAL( reaction_ordering.size(), 1 );
+  FRENSIE_REQUIRE( reaction_ordering.find(5) != reaction_ordering.end() );
+  FRENSIE_CHECK_EQUAL( reaction_ordering.find(5)->second, 0 );
+}
+
+//---------------------------------------------------------------------------//
+// Check that the reactions that undergo scattering in the CM frame have
+// been extracted correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionCMScattering )
+{
+  auto reaction_cm_scattering =
+    photonuclear_distribution_factory_c12->getReactionCMScattering();
+  
+  // Only 1 reaction produces an outgoing photon, reaction 5
+  FRENSIE_REQUIRE_EQUAL( reaction_cm_scattering.size(), 1 );
+  FRENSIE_REQUIRE( reaction_cm_scattering.find(5) != reaction_cm_scattering.end() );
+  FRENSIE_CHECK_EQUAL( reaction_cm_scattering.find(5)->second, true );
+}
+
+//---------------------------------------------------------------------------//
+// Check that the reactions that undergo isotropic scattering have been
+// extracted correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionWithIsotropicScatteringOnly )
+{
+  auto reactions_with_isotropic_scattering =
+    photonuclear_distribution_factory_c12->getReactionsWithIsotropicScatteringOnly();
+  
+  // Only 1 reaction produces an outgoing photon, reaction 5. This reaction has
+  // dist_index = 0 -> iso scat only
+  FRENSIE_REQUIRE_EQUAL( reactions_with_isotropic_scattering.size(), 1 );
+  FRENSIE_CHECK( reactions_with_isotropic_scattering.find( MonteCarlo::GAMMA__ANYTHING_REACTION ) != reactions_with_isotropic_scattering.end() );
+}
+
+//---------------------------------------------------------------------------//
+// Check that the reactions with coupled energy-angle scattering distributions
+// have been extracted correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionWithCoupledEnergyAngleDist )
+{
+  auto reactions_with_coupled_energy_angle_dist =
+    photonuclear_distribution_factory_c12->getReactionsWithCoupledEnergyAngleDist();
+  
+  // Only 1 reaction produces an outgoing photon, reaction 5. This reaction has
+  // dist_index = 0 -> no Energy Angle dependence
+  FRENSIE_CHECK_EQUAL( reactions_with_coupled_energy_angle_dist.size(), 0 );
+}
+
+//---------------------------------------------------------------------------//
+// Check that angular distributions have been extracted correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionAngularDist )
+{
+  auto reaction_angular_distributions =
+    photonuclear_distribution_factory_c12->getReactionAngularDist();
+
+  FRENSIE_CHECK_EQUAL( reaction_angular_distributions.size(), 0 );
+}
+
+//---------------------------------------------------------------------------//
+// Check that the angular distribution start indices hae been extracted
+// correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionAngularDistStartIndex )
+{
+  auto reaction_angular_distribution_start_index =
+    photonuclear_distribution_factory_c12->getReactionAngularDistStartIndex();
+
+  FRENSIE_CHECK_EQUAL( reaction_angular_distribution_start_index.size(), 0 );
+}
+
+//---------------------------------------------------------------------------//
+// Check that the reaction energy distributions have been extracted
+// correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionEnergyDist )
+{
+  auto reaction_energy_dists =
+    photonuclear_distribution_factory_c12->getReactionEnergyDist();
+
+  FRENSIE_REQUIRE_EQUAL( reaction_energy_dists.size(), 1 );
+  FRENSIE_REQUIRE( reaction_energy_dists.find( MonteCarlo::GAMMA__ANYTHING_REACTION ) != reaction_energy_dists.end() );
+
+  auto energy_dist =
+    reaction_energy_dists.find( MonteCarlo::GAMMA__ANYTHING_REACTION )->second;
+
+  FRENSIE_CHECK_EQUAL( energy_dist.size(), 11739 );
+  FRENSIE_CHECK_EQUAL( energy_dist.front(), 0 );
+  FRENSIE_CHECK_EQUAL( energy_dist.back(), 1 );
+}
+
+//---------------------------------------------------------------------------//
+// Check that the reaction energy distribution start indices habe been
+// extracted correctly
+FRENSIE_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
+		   getReactionEnergyDistStartIndex )
+{
+  auto reaction_energy_dist_start_indices =
+    photonuclear_distribution_factory_c12->getReactionEnergyDistStartIndex();
+
+  FRENSIE_REQUIRE_EQUAL( reaction_energy_dist_start_indices.size(), 1 );
+  FRENSIE_REQUIRE( reaction_energy_dist_start_indices.find( MonteCarlo::GAMMA__ANYTHING_REACTION ) != reaction_energy_dist_start_indices.end() );
+  FRENSIE_CHECK_EQUAL( reaction_energy_dist_start_indices.find( MonteCarlo::GAMMA__ANYTHING_REACTION )->second, 0 );
+}
+
+//---------------------------------------------------------------------------//
+// Custom Setup
+//---------------------------------------------------------------------------//
+FRENSIE_CUSTOM_UNIT_TEST_SETUP_BEGIN();
+
+std::string test_ace_file_name, test_ace_table_name;
+
+FRENSIE_CUSTOM_UNIT_TEST_COMMAND_LINE_OPTIONS()
+{
+  ADD_STANDARD_OPTION_AND_ASSIGN_VALUE( "test_ace_file",
+                                        test_ace_file_name, "",
+                                        "Test ACE file name" );
+  ADD_STANDARD_OPTION_AND_ASSIGN_VALUE( "test_ace_table",
+                                        test_ace_table_name, "",
+                                        "Test ACE table name" );
+}
+
+FRENSIE_CUSTOM_UNIT_TEST_INIT()
+{
+  // Create a file handler and data extractor
+  std::shared_ptr<Data::ACEFileHandler> ace_file_handler(
+				 new Data::ACEFileHandler( test_ace_file_name,
+							   test_ace_table_name,
+							   1u ) );
+
+  xss_data_extractor.reset( new Data::XSSPhotonuclearDataExtractor(
+				      ace_file_handler->getTableNXSArray(),
+				      ace_file_handler->getTableJXSArray(),
+				      ace_file_handler->getTableXSSArray() ) );
 
   photonuclear_distribution_factory_c12.reset(
     new TestPhotonuclearScatteringDistributionACEFactory(
-                             test_basic_c12_ace_table_name,
-                             ace_file_handler_c12->getTableAtomicWeightRatio(),
-			     *xss_data_extractor_c12 ) );
-
-  // Only 1 reaction produces an outgoing photon, reaction 5
-  TEST_COMPARE( photonuclear_distribution_factory_c12->getReactionOrdering().size() ,==, 1);
-
-  TEST_EQUALITY( photonuclear_distribution_factory_c12->getReactionOrdering().find(5)->second , 0);
-
+                                 test_ace_table_name,
+                                 ace_file_handler->getTableAtomicWeightRatio(),
+                                 *xss_data_extractor ) );
 }
 
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionCMScattering_c12 )
-{
-  // Only 1 reaction produces an outgoing photon, reaction 5
-  TEST_COMPARE( photonuclear_distribution_factory_c12->getReactionCMScattering().size() ,==, 1);
-
-  TEST_EQUALITY( photonuclear_distribution_factory_c12->getReactionCMScattering().find(5)->second , true);
-
-}
-
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionWithIsotropicScatteringOnly_c12 )
-{
-  // Only 1 reaction produces an outgoing photon, reaction 5. This reaction has dist_index = 0 -> iso scat only
-  TEST_COMPARE( photonuclear_distribution_factory_c12->getReactionsWithIsotropicScatteringOnly().size() ,==, 1);
-
-  TEST_ASSERT( photonuclear_distribution_factory_c12->getReactionsWithIsotropicScatteringOnly().find(MonteCarlo::GAMMA__ANYTHING_REACTION) != photonuclear_distribution_factory_c12->getReactionsWithIsotropicScatteringOnly().end());
-
-}
-
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionWithCoupledEnergyAngleDist_c12 )
-{
-  // Only 1 reaction produces an outgoing photon, reaction 5. This reaction has dist_index = 0 -> no Energy Angle dependence
-  TEST_COMPARE( photonuclear_distribution_factory_c12->getReactionsWithCoupledEnergyAngleDist().size() ,==, 0);
-}
-
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionAngularDist_c12 )
-{
-TEST_COMPARE( photonuclear_distribution_factory_c12->getReactionAngularDist().size() ,==, 0 );
-
-  TEST_ASSERT( photonuclear_distribution_factory_c12->getReactionAngularDist().find(MonteCarlo::GAMMA__ANYTHING_REACTION) == photonuclear_distribution_factory_c12->getReactionAngularDist().end());
-
-}
-
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionAngularDistStartIndex_c12 )
-{
-  TEST_COMPARE( photonuclear_distribution_factory_c12->getReactionAngularDistStartIndex().size() ,==, 0 );
-
-}
-
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionEnergyDist_c12 )
-{
-
-  TEST_COMPARE(  photonuclear_distribution_factory_c12->getReactionEnergyDist().find(MonteCarlo::GAMMA__ANYTHING_REACTION)->second.size() ,==, 11739 );
-
-  TEST_COMPARE(  photonuclear_distribution_factory_c12->getReactionEnergyDist().find(MonteCarlo::GAMMA__ANYTHING_REACTION)->second.front() ,==, 0 );
-
-  TEST_COMPARE(  photonuclear_distribution_factory_c12->getReactionEnergyDist().find(MonteCarlo::GAMMA__ANYTHING_REACTION)->second.back() ,==, 1 );
-
-}
-
-TEUCHOS_UNIT_TEST( PhotonuclearScatteringDistributionACEFactory,
-		   getReactionEnergyDistStartIndex_c12 )
-{
-
-  TEST_COMPARE(  photonuclear_distribution_factory_c12->getReactionEnergyDistStartIndex().size() ,==, 1 );
-
-  TEST_COMPARE(  photonuclear_distribution_factory_c12->getReactionEnergyDistStartIndex().find(MonteCarlo::GAMMA__ANYTHING_REACTION)->second ,==, 0 );
-
-}
-//---------------------------------------------------------------------------//
-// Custom main function
-//---------------------------------------------------------------------------//
-int main( int argc, char** argv )
-{
-  Teuchos::CommandLineProcessor & clp = Teuchos::UnitTestRepository::getCLP();
-
-  clp.setOption( "test_basic_c12_ace_file",
-		 &test_basic_c12_ace_file_name,
-		 "Test basic C12 ACE file name" );
-  clp.setOption( "test_basic_c12_ace_table",
-		 &test_basic_c12_ace_table_name,
-		 "Test basic C12 ACE table name in basic ACE file" );
-
-  // Initialize the random number generator
-  Utility::RandomNumberGenerator::createStreams();
-
-  Teuchos::GlobalMPISession mpiSession( &argc, &argv );
-  return Teuchos::UnitTestRepository::runUnitTestsFromMain( argc, argv );
-}
+FRENSIE_CUSTOM_UNIT_TEST_SETUP_END();
 
 //---------------------------------------------------------------------------//
 // end tstPhotonuclearScatteringDistributionACEFactory.cpp
