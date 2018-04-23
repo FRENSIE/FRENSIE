f8270fac3d5eb4c88e481af6b34027475a52df5b
Alex Robinson <aprobinson@wisc.edu>
2018-04-23 14:41:21 -0500

    Finished updating the NeutronMaterial and NuclideFactory tests using the FRENSIE unit test harness.

--- packages/monte_carlo/collision/neutron/test/tstNuclideFactory.cpp
+++ packages/monte_carlo/collision/neutron/test/tstNuclideFactory.cpp
@@ -10,22 +10,25 @@
 #include <iostream>
 #include <memory>
 
-// Trilinos Includes
-#include <Teuchos_UnitTestHarness.hpp>
-#include <Teuchos_RCP.hpp>
-#include <Teuchos_XMLParameterListCoreHelpers.hpp>
-#include <Teuchos_VerboseObject.hpp>
-
 // FRENSIE Includes
 #include "MonteCarlo_NuclideFactory.hpp"
-#include "Utility_UnitTestHarnessExtensions.hpp"
+#include "MonteCarlo_DecoupledPhotonProductionNuclide.hpp"
+#include "Data_ScatteringCenterPropertiesDatabase.hpp"
+#include "Utility_UnitTestHarnessWithMain.hpp"
+
+//---------------------------------------------------------------------------//
+// Testing Types
+//---------------------------------------------------------------------------//
+
+using boost::units::si::kelvin;
+using Utility::Units::MeV;
 
 //---------------------------------------------------------------------------//
 // Testing Variables.
 //---------------------------------------------------------------------------//
-std::string test_cross_sections_xml_directory;
 
-Teuchos::ParameterList cross_section_table_info;
+std::unique_ptr<const boost::filesystem::path> data_directory;
+std::unique_ptr<MonteCarlo::ScatteringCenterDefinitionDatabase> nuclide_definitions;
 
 std::shared_ptr<const MonteCarlo::SimulationProperties> properties;
 
@@ -33,79 +36,196 @@ std::shared_ptr<const MonteCarlo::SimulationProperties> properties;
 // Tests.
 //---------------------------------------------------------------------------//
 // Check that a factory can be constructed
-TEUCHOS_UNIT_TEST( NuclideFactory, constructor )
+FRENSIE_UNIT_TEST( NuclideFactory, constructor )
 {
-  std::unordered_set<std::string> nuclide_aliases;
-  nuclide_aliases.insert( "H-1_293.6K" );
-  nuclide_aliases.insert( "H-1_300K" );
-  nuclide_aliases.insert( "H-1_900K" );
+  MonteCarlo::NuclideFactory::ScatteringCenterNameSet nuclide_names;
+  nuclide_names.insert( "H-1_293.6K" );
+  nuclide_names.insert( "H-1_300K" );
+  nuclide_names.insert( "H-1_900K" );
 
-  std::shared_ptr<MonteCarlo::NuclideFactory> factory;
+  MonteCarlo::SimulationProperties properties;
+  properties.setNumberOfNeutronHashGridBins( 100 );
+  properties.setUnresolvedResonanceProbabilityTableModeOff();
+  properties.setParticleMode( MonteCarlo::NEUTRON_MODE );
 
-  TEST_NOTHROW( factory.reset( new MonteCarlo::NuclideFactory(
-                                             test_cross_sections_xml_directory,
-                                             cross_section_table_info,
-                                             nuclide_aliases,
-                                             *properties ) ) );
+  std::unique_ptr<MonteCarlo::NuclideFactory> factory;
+
+  FRENSIE_CHECK_NO_THROW( factory.reset( new MonteCarlo::NuclideFactory(
+                                                          *data_directory,
+                                                          nuclide_names,
+                                                          *nuclide_definitions,
+                                                          properties,
+                                                          true ) ) );
 }
 
 //---------------------------------------------------------------------------//
 // Check that a nuclide map can be created
-TEUCHOS_UNIT_TEST( NuclideFactory, createNuclideMap )
+FRENSIE_UNIT_TEST( NuclideFactory, createNuclideMap_basic )
 {
-  std::unordered_set<std::string> nuclide_aliases;
-  nuclide_aliases.insert( "H-1_293.6K" );
-  nuclide_aliases.insert( "H-1_300K" );
-  nuclide_aliases.insert( "H-1_900K" );
+  MonteCarlo::NuclideFactory::ScatteringCenterNameSet nuclide_names;
+  nuclide_names.insert( "H-1_293.6K" );
+  nuclide_names.insert( "H-1_300K" );
+  nuclide_names.insert( "H-1_900K" );
 
-  std::shared_ptr<MonteCarlo::NuclideFactory> factory(
+  MonteCarlo::SimulationProperties properties;
+  properties.setNumberOfNeutronHashGridBins( 100 );
+  properties.setUnresolvedResonanceProbabilityTableModeOff();
+  properties.setParticleMode( MonteCarlo::NEUTRON_MODE );
+
+  std::unique_ptr<MonteCarlo::NuclideFactory> factory(
                                       new MonteCarlo::NuclideFactory(
-                                             test_cross_sections_xml_directory,
-                                             cross_section_table_info,
-                                             nuclide_aliases,
-                                             *properties ) );
+                                                          *data_directory,
+                                                          nuclide_names,
+                                                          *nuclide_definitions,
+                                                          properties,
+                                                          true ) );
   
-  std::unordered_map<std::string,Teuchos::RCP<MonteCarlo::Nuclide> > nuclide_map;
+  MonteCarlo::NuclideFactory::NuclideNameMap nuclide_map;
 
   factory->createNuclideMap( nuclide_map );
 
-  TEST_EQUALITY_CONST( nuclide_map.size(), 3 );
-  TEST_ASSERT( nuclide_map.count( "H-1_293.6K" ) );
-  TEST_ASSERT( !nuclide_map["H-1_293.6K"].is_null() );
-  TEST_ASSERT( nuclide_map.count( "H-1_300K" ) );
-  TEST_ASSERT( !nuclide_map["H-1_300K"].is_null() );
-  TEST_ASSERT( nuclide_map.count( "H-1_900K" ) );
-  TEST_ASSERT( !nuclide_map["H-1_900K"].is_null() );
+  FRENSIE_CHECK_EQUAL( nuclide_map.size(), 3 );
+  FRENSIE_CHECK( nuclide_map.count( "H-1_293.6K" ) );
+  FRENSIE_CHECK( nuclide_map["H-1_293.6K"].get() != NULL );
+  FRENSIE_CHECK( nuclide_map.count( "H-1_300K" ) );
+  FRENSIE_CHECK( nuclide_map["H-1_300K"].get() != NULL );
+  FRENSIE_CHECK( nuclide_map.count( "H-1_900K" ) );
+  FRENSIE_CHECK( nuclide_map["H-1_900K"].get() != NULL );
+}
+
+// //---------------------------------------------------------------------------//
+// // Check that a nuclide map can be created
+// FRENSIE_UNIT_TEST( NuclideFactory, createNuclideMap_unresolved_resonances )
+// {
+
+// }
+
+//---------------------------------------------------------------------------//
+// Check that a nuclide map can be created
+FRENSIE_UNIT_TEST( NuclideFactory, createNuclideMap_photon_production )
+{
+  MonteCarlo::NuclideFactory::ScatteringCenterNameSet nuclide_names;
+  nuclide_names.insert( "H-1_293.6K" );
+  nuclide_names.insert( "H-1_300K" );
+  nuclide_names.insert( "H-1_900K" );
+
+  MonteCarlo::SimulationProperties properties;
+  properties.setNumberOfNeutronHashGridBins( 100 );
+  properties.setUnresolvedResonanceProbabilityTableModeOff();
+  properties.setParticleMode( MonteCarlo::NEUTRON_PHOTON_MODE );
+
+  std::unique_ptr<MonteCarlo::NuclideFactory> factory(
+                                      new MonteCarlo::NuclideFactory(
+                                                          *data_directory,
+                                                          nuclide_names,
+                                                          *nuclide_definitions,
+                                                          properties,
+                                                          true ) );
+  
+  MonteCarlo::NuclideFactory::NuclideNameMap nuclide_map;
+
+  factory->createNuclideMap( nuclide_map );
+
+  FRENSIE_CHECK_EQUAL( nuclide_map.size(), 3 );
+  
+  FRENSIE_CHECK( nuclide_map.count( "H-1_293.6K" ) );
+  FRENSIE_REQUIRE( nuclide_map["H-1_293.6K"].get() != NULL );
+
+  const MonteCarlo::DecoupledPhotonProductionNuclide* true_nuclide =
+    dynamic_cast<const MonteCarlo::DecoupledPhotonProductionNuclide*>(
+                                             nuclide_map["H-1_293.6K"].get() );
+
+  FRENSIE_CHECK( true_nuclide != NULL );
+  
+  FRENSIE_CHECK( nuclide_map.count( "H-1_300K" ) );
+  FRENSIE_REQUIRE( nuclide_map["H-1_300K"].get() != NULL );
+
+  true_nuclide =
+    dynamic_cast<const MonteCarlo::DecoupledPhotonProductionNuclide*>(
+                                               nuclide_map["H-1_300K"].get() );
+
+  FRENSIE_CHECK( true_nuclide != NULL );
+  
+  FRENSIE_CHECK( nuclide_map.count( "H-1_900K" ) );
+  FRENSIE_REQUIRE( nuclide_map["H-1_900K"].get() != NULL );
+
+  true_nuclide =
+    dynamic_cast<const MonteCarlo::DecoupledPhotonProductionNuclide*>(
+                                               nuclide_map["H-1_900K"].get() );
+
+  FRENSIE_CHECK( true_nuclide != NULL );
 }
 
 //---------------------------------------------------------------------------//
-// Custom setup
+// Custom Setup
 //---------------------------------------------------------------------------//
-UTILITY_CUSTOM_TEUCHOS_UNIT_TEST_SETUP_BEGIN();
+FRENSIE_CUSTOM_UNIT_TEST_SETUP_BEGIN();
 
-UTILITY_CUSTOM_TEUCHOS_UNIT_TEST_COMMAND_LINE_OPTIONS()
+std::string test_scattering_center_database_name;
+
+FRENSIE_CUSTOM_UNIT_TEST_COMMAND_LINE_OPTIONS()
 {
-  clp().setOption( "test_cross_sections_xml_directory",
-                   &test_cross_sections_xml_directory,
-                   "Test cross_sections.xml file name" );
+  ADD_STANDARD_OPTION_AND_ASSIGN_VALUE( "test_database",
+                                        test_scattering_center_database_name, "",
+                                        "Test scattering center database name "
+                                        "with path" );
 }
 
-UTILITY_CUSTOM_TEUCHOS_UNIT_TEST_DATA_INITIALIZATION()
+FRENSIE_CUSTOM_UNIT_TEST_INIT()
 {
-  // Assign the name of the cross_sections.xml file with path
-  std::string cross_section_xml_file = test_cross_sections_xml_directory;
-  cross_section_xml_file += "/cross_sections.xml";
+  // Determine the database directory
+  boost::filesystem::path database_path =
+    test_scattering_center_database_name;
+  
+  data_directory.reset(
+                  new boost::filesystem::path( database_path.parent_path() ) );
 
-  // Read in the xml file storing the cross section table information
-  Teuchos::updateParametersFromXmlFile(
-			         cross_section_xml_file,
-			         Teuchos::inoutArg(cross_section_table_info) );
+  // Load the database
+  const Data::ScatteringCenterPropertiesDatabase database( database_path );
 
-  // Create the simulation properties
-  properties.reset( new MonteCarlo::SimulationProperties );
+  const Data::NuclideProperties& h1_properties =
+    database.getNuclideProperties( 1001 );
+
+  // Initialize the nuclide definitions
+  nuclide_definitions.reset( new MonteCarlo::ScatteringCenterDefinitionDatabase );
+
+  MonteCarlo::ScatteringCenterDefinition& h1_293K_definition =
+    nuclide_definitions->createDefinition( "H-1_293.6K", 1001 );
+
+  h1_293K_definition.setNuclearDataProperties(
+                                 h1_properties.getSharedNuclearDataProperties(
+                                         Data::NuclearDataProperties::ACE_FILE,
+                                         7,
+                                         2.53010E-08*MeV,
+                                         true ) );
+
+  // There is no test data for 300K so we'll just use the closest evaluation
+  MonteCarlo::ScatteringCenterDefinition& h1_300K_definition =
+    nuclide_definitions->createDefinition( "H-1_300K", 1001 );
+
+  h1_300K_definition.setNuclearDataProperties(
+                                 h1_properties.getSharedNuclearDataProperties(
+                                         Data::NuclearDataProperties::ACE_FILE,
+                                         7,
+                                         300*kelvin,
+                                         false ) );
+
+  // There is no test data for 900K so we'll just use the closest evaluation
+  MonteCarlo::ScatteringCenterDefinition& h1_900K_definition =
+    nuclide_definitions->createDefinition( "H-1_900K", 1001 );
+
+  h1_900K_definition.setNuclearDataProperties(
+                                 h1_properties.getSharedNuclearDataProperties(
+                                         Data::NuclearDataProperties::ACE_FILE,
+                                         7,
+                                         900*kelvin,
+                                         false ) );
+
+  // Initialize the random number generator
+  Utility::RandomNumberGenerator::createStreams();
 }
 
-UTILITY_CUSTOM_TEUCHOS_UNIT_TEST_SETUP_END();
+FRENSIE_CUSTOM_UNIT_TEST_SETUP_END();
 
 //---------------------------------------------------------------------------//
 // end tstNuclideFactory.cpp
