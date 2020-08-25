d4a11120ca601759cefd262a096808c50d204251
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-08-25 15:34:28 -0500

    what is my answer ?

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,10 +4,11 @@ source draft.sh
 source detect_package.sh
 
 draft_pr=`is_draft`
-
+echo "Draft: $draft_pr"
 if [[ "${draft_pr}" == 1 ]]; then
     uniq_modified_pkg=`uniq_modified_pkg`
     for pkg in ${uniq_modified_pkg}; do
+        echo "make $pkg; make ${pkg}-test"
         make ${pkg}
         make ${pkg}-test
     done
