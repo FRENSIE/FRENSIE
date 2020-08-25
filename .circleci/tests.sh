f6ac81154bd346e3219349bc7956b146e91e8175
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-08-25 12:23:01 -0500

    first setup of CI

---
+++ .circleci/tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source draft.sh
+source detect_package.sh
+
+draft_pr=`is_draft`
+
+if [[ "${draft_pr}" == 1 ]]; then
+    uniq_modified_pkg=`uniq_modified_pkg`
+    for pkg in ${uniq_modified_pkg}; do
+        make test-${pkg}
+    done
+else
+    make test
+fi
