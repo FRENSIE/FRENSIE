e6260ab923cbb03db7e4fe4dc0533313c981ea35
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-09-01 10:26:54 -0500

    un comment the darft part

--- .circleci/draft.sh
+++ .circleci/draft.sh
@@ -9,43 +9,42 @@ function is_draft() {
         "CIRCLE_TOKEN"
         "CIRCLE_BUILD_NUM"
     )
-    # # check if required VAR are defined 
-    # for required_env_var in ${required_env_vars[@]}; do
-    #     if [[ -z "${!required_env_var}" ]]; then
-    #     printf "BAD ENVIRONMENT SETTINGS\n"
-    #     printf "${required_env_var} not provided, but that doesn't mean we should skip CI.\n"
-    #     exit 0
-    #     fi
-    # done
+    # check if required VAR are defined 
+    for required_env_var in ${required_env_vars[@]}; do
+        if [[ -z "${!required_env_var}" ]]; then
+        printf "BAD ENVIRONMENT SETTINGS\n"
+        printf "${required_env_var} not provided, but that doesn't mean we should skip CI.\n"
+        exit 0
+        fi
+    done
 
 
-    # # Since we're piggybacking off of an existing OAuth var, tweak the var for our uses
-    # token=$(printf "${GITHUB_TOKEN}" | cut -d':' -f1)
-    # headers="Authorization: token $token"
-    # api_endpoint="https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PR_REPONAME}/pulls/${CIRCLE_PR_NUMBER}"
-    # # Fetch PR metadata from Github's API and parse fields from json
-    # github_res=$(curl --silent --header "${headers}" "${api_endpoint}" | jq '{mergeable_state: .mergeable_state, title: .title}')
-    # mergeable_state=$(printf "${github_res}" | jq '.mergeable_state')
-    # title=$(printf "${github_res}" | jq '.title' | tr '[:upper:]' '[:lower:]')
-    # echo "${title}"
-    # if [[ "${title}" == "null" && "${mergeable_state}" == "null" ]]; then
-    #     printf "Couldn't fetch info on PR, but that doesn't mean we should skip CI.\n"
-    #     exit 0
-    # fi
+    # Since we're piggybacking off of an existing OAuth var, tweak the var for our uses
+    token=$(printf "${GITHUB_TOKEN}" | cut -d':' -f1)
+    headers="Authorization: token $token"
+    api_endpoint="https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PR_REPONAME}/pulls/${CIRCLE_PR_NUMBER}"
+    # Fetch PR metadata from Github's API and parse fields from json
+    github_res=$(curl --silent --header "${headers}" "${api_endpoint}" | jq '{mergeable_state: .mergeable_state, title: .title}')
+    mergeable_state=$(printf "${github_res}" | jq '.mergeable_state')
+    title=$(printf "${github_res}" | jq '.title' | tr '[:upper:]' '[:lower:]')
+    echo "${title}"
+    if [[ "${title}" == "null" && "${mergeable_state}" == "null" ]]; then
+        printf "Couldn't fetch info on PR, but that doesn't mean we should skip CI.\n"
+        exit 0
+    fi
 
-    # # Check for Draft
-    # draft_pr=0
-    # if [[ "${mergeable_state}" == "\"draft\"" ]]; then
-    #     printf "PR is a draft, skipping CI!\n"
-    #     draft_pr=1
-    # fi
-    # # Check for Tittle SKIP Token
-    # for skip_token in '[wip]' '[WIP]'; do
-    #     if [[ ${title} == *"${skip_token}"* ]]; then
-    #     printf "Found \"${skip_token}\" in PR title, skipping CI!\n"
-    #     draft_pr=1
-    #     fi
-    # done
-    draft_pr=1
+    # Check for Draft
+    draft_pr=0
+    if [[ "${mergeable_state}" == "\"draft\"" ]]; then
+        printf "PR is a draft, skipping CI!\n"
+        draft_pr=1
+    fi
+    # Check for Tittle SKIP Token
+    for skip_token in '[wip]' '[WIP]'; do
+        if [[ ${title} == *"${skip_token}"* ]]; then
+        printf "Found \"${skip_token}\" in PR title, skipping CI!\n"
+        draft_pr=1
+        fi
+    done
     echo $draft_pr
 }
