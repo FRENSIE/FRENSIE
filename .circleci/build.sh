094c0839e9868807cca058e7b34694f575907dfa
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-09-01 14:45:44 -0500

    printing the lsit of package to build

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -8,10 +8,11 @@ echo "Draft: $draft_pr"
 if [[ "${draft_pr}" == 1 ]]; then
     echo "In draft Mode"
     uniq_modified_pkg=`uniq_modified_pkg`
+    echo "package to be build: $uniq_modified_pkg"
     for pkg in ${uniq_modified_pkg}; do
-        echo "make $pkg; make ${pkg}-test"
+        echo "make $pkg; make ${pkg}_test"
         make ${pkg}
-        make ${pkg}-tests
+        make ${pkg}_tests
     done
 else
     make
