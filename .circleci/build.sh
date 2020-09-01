32eba518217989692e59fa5514bf630792cf306e
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-09-01 13:23:40 -0500

    removing unused echo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-jq --help
-
 source /root/frensie/.circleci/draft.sh
 source /root/frensie/.circleci/detect_package.sh
 
 draft_pr=`is_draft`
 echo "Draft: $draft_pr"
-# if [[ "${draft_pr}" == 1 ]]; then
-#     uniq_modified_pkg=`uniq_modified_pkg`
-#     for pkg in ${uniq_modified_pkg}; do
-#         echo "make $pkg; make ${pkg}-test"
-#         make ${pkg}
-#         make ${pkg}_tests
-#     done
-# else
-#     make
-# fi
+if [[ "${draft_pr}" == 1 ]]; then
+    uniq_modified_pkg=`uniq_modified_pkg`
+    for pkg in ${uniq_modified_pkg}; do
+        echo "make $pkg; make ${pkg}-test"
+        make ${pkg}
+        make ${pkg}_tests
+    done
+else
+    make
+fi
