7562aecf6f96ddd1aacfc6495bc07d07f1a28824
Lewis Gross <ligross@wisc.edu>
2020-09-29 16:07:07 -0500

    need a test to ensure new logic works, to implement

---
+++ packages/data/ace/test/tstXSSNeutronDataExtractorRecursiveIndexFinder.cpp
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   tstXSSNeutronDataExtractorRecursiveIndexFinder.cpp
+//! \author Lewis Gross
+//! \brief  checks if ecursive algorithm finds appropriate next nonzero index
+//!
+//---------------------------------------------------------------------------//
+
+// pick some isotopes at different temperatures, for sure pick 18040.710nc and check
+// determine all blocks included
+// 
+// test all blocks extraction
+// that the index finder finds the correct index and gives the correct size and extracts
+// the correct blocks
+
+// need to add this file to CMakeLists.txt
